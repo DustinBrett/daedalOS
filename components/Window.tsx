d339335686ec2d5686b06b23561fc988130ace61
Dustin Brett <dustin@voxter.ca>
2020-08-11 00:48:34 -0700

    Tweaks

--- components/Window.tsx
+++ components/Window.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 import Draggable from 'react-draggable';
+import { Resizable } from 'react-resizable';
 
 import styles from '../styles/Window.module.scss';
 
@@ -7,21 +10,33 @@ export type WindowType = {
   title: string
 };
 
-// TODO: Resizeable
-
 export function Window({ title }: WindowType) {
+  const [height, setHeight] = useState(250),
+    [width, setWidth] = useState(250),
+    onResize = (resizeEvent, { size: { width, height } }) => {
+      setHeight(height);
+      setWidth(width);
+    };
+
   return (
-    <Draggable>
-      <div className={ styles.window }>
-        <div className={ styles.title_bar }>
-          <span>{ title }</span>
-          <span>
-            <span className={ styles.minimize }></span>
-            <span className={ styles.maximize }></span>
-            <span className={ styles.close }></span>
-          </span>
+    <Draggable handle='.handle'>
+      <Resizable
+        height={ height }
+        minConstraints={[150, 150]}
+        onResize={ onResize }
+        width={ width }
+      >
+        <div className={ styles.window } style={{ height: `${ height }px`, width: `${ width }px` }}>
+          <div className={ `${ styles.title_bar } handle` }>
+            <span>{ title }</span>
+            <span>
+              <span className={ styles.minimize }></span>
+              <span className={ styles.maximize }></span>
+              <span className={ styles.close }></span>
+            </span>
+          </div>
         </div>
-      </div>
+      </Resizable>
     </Draggable>
   );
 };
