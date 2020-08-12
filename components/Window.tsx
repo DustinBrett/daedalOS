4b8da5be231147605fb74cfdd9c407d0e010515c
Dustin Brett <dustin@voxter.ca>
2020-08-11 22:37:36 -0700

    Tweaks

--- components/Window.tsx
+++ components/Window.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinusCircle, faPlusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 import Draggable from 'react-draggable';
 import { Resizable } from 'react-resizable';
@@ -11,30 +13,30 @@ export type WindowType = {
 };
 
 export function Window({ title }: WindowType) {
-  const [height, setHeight] = useState(250),
+  const [height, setHeight] = useState(200),
     [width, setWidth] = useState(250),
     onResize = (resizeEvent: any, { size: { width, height } }: any) => { // TODO: Types
       setHeight(height);
       setWidth(width);
     };
 
-  // TODO: Resize from all directions
+  // TODO: Resize from all directions or use diff lib
   return (
     <Draggable handle='.handle'>
       <Resizable
         height={ height }
-        minConstraints={[150, 150]}
+        minConstraints={ [200, 150] }
         onResize={ onResize }
         width={ width }
       >
         <div className={ styles.window } style={{ height: `${ height }px`, width: `${ width }px` }}>
           <div className={ `${ styles.title_bar } handle` }>
-            <span>{ title }</span>
-            <span>
-              <span className={ styles.minimize }></span>
-              <span className={ styles.maximize }></span>
-              <span className={ styles.close }></span>
-            </span>
+            <div className={ styles.title }>{ title }</div>
+            <div className={ styles.actions }>
+              <FontAwesomeIcon className={ styles.minimize } icon={ faMinusCircle } />
+              <FontAwesomeIcon className={ styles.maximize } icon={ faPlusCircle } />
+              <FontAwesomeIcon className={ styles.close } icon={ faTimesCircle } />
+            </div>
           </div>
         </div>
       </Resizable>
