8ae9aa122bc028ea150484e9f601c92547fad583
Dustin Brett <dustin@voxter.ca>
2020-08-14 14:53:34 -0700

    Tweaks

--- dist/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
