9d28307b7452002dbce00c062967d3c99ec11eb9
Dustin Brett <dustin@voxter.ca>
2020-08-14 23:38:21 -0700

    Tweaks

--- components/Taskbar.tsx
+++ components/Taskbar.tsx
@@ -1,8 +1,29 @@
 import styles from '../styles/Taskbar.module.scss';
 import { useContext } from 'react';
 import { AppsContext } from '../resources/AppsProvider';
+import posed, { PoseGroup } from 'react-pose';
 import Clock from './Clock';
 import TaskbarEntry from './TaskbarEntry';
+
+const PosedDiv = posed.div({
+  enter: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      x: {
+        type: 'spring'
+      }
+    }
+  },
+  exit: {
+    opacity: 0,
+    opacity: 0,
+    transition: {
+      duration: 300
+    },
+    x: -50
+  }
+});
 
 export default function Taskbar() {
   const { apps = {} } = useContext(AppsContext);
@@ -10,11 +31,15 @@ export default function Taskbar() {
   return (
     <div className={ styles.taskbar }>
       <div className={ styles.taskbar_entries }>
-        { Object.entries(apps)
-          .filter(([_id, app]) => app.opened)
-          .map(([id, app]) => (
-            <TaskbarEntry key={ id } icon={ app.icon } id={ id } name={ app.name } />
-        )) }
+        <PoseGroup animateOnMount={ true }>
+          { Object.entries(apps)
+            .filter(([_id, app]) => app.opened)
+            .map(([id, app]) => (
+              <PosedDiv key={ id }>
+                <TaskbarEntry icon={ app.icon } id={ id } name={ app.name } />
+              </PosedDiv>
+          )) }
+        </PoseGroup>
       </div>
       <Clock hour12={ true } />
     </div>
