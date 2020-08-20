5c92f73ba06c93f8602c72f96469de37a47a0ea6
Dustin Brett <dustin@voxter.ca>
2020-08-20 00:37:35 -0700

    Tweaks

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,10 +1,17 @@
 import type { FC } from 'react';
 import type { App } from '../contexts/Apps';
+import { AppsContext } from '../contexts/Apps';
+import { useContext } from 'react';
 import styles from '../styles/Icon.module.scss';
 
-export const Icon: FC<App> = ({ icon, name }) => (
-  <li className={styles.icon}>
-    {icon}
-    {name}
-  </li>
-);
+export const Icon: FC<App> = ({ icon, id, name }) => {
+  const { apps, updateApps } = useContext(AppsContext),
+    openApp = () => updateApps(apps.map(app => app.id === id ? { ...app, running: true } : app));
+
+  return (
+    <li className={styles.icon} onDoubleClick={openApp}>
+      {icon}
+      {name}
+    </li>
+  );
+};
