50e0bb3f1ecdff8d46096b94633d67571625ae40
Dustin Brett <dustin@voxter.ca>
2020-08-23 17:41:04 -0700

    Tweaks

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react';
 import { name } from '../package.json';
 import Head from 'next/head';
 
+// TODO: Favicon
 export const MetaData: FC = () => (
   <Head>
     <title>{name}</title>
---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import type { ReactElement } from 'react';
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    return await Document.getInitialProps(ctx);
+  }
+
+  // TODO: i18n & dynamic lang
+  render(): ReactElement {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
