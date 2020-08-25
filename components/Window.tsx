82cb56850e5b3e6fc7c69c8ce916d06fb70e5502
Dustin Brett <dustin@voxter.ca>
2020-08-24 23:49:10 -0700

    Tweaks

--- components/Window.tsx
+++ components/Window.tsx
@@ -3,16 +3,22 @@ import styles from '../styles/Window.module.scss';
 
 type Window = {
   name: string;
+  onMinimize: () => void;
+  onClose: () => void;
 };
 
-export const Window: FC<Window> = ({ children, name }) => (
+export const Window: FC<Window> = ({ children, name, onMinimize, onClose }) => (
   <li className={styles.window}>
     <header>
       <h1>{name}</h1>
       <nav>
-        <button id={styles.minimize}>-</button>
+        <button id={styles.minimize} onClick={onMinimize}>
+          -
+        </button>
         <button id={styles.maximize}>+</button>
-        <button id={styles.close}>x</button>
+        <button id={styles.close} onClick={onClose}>
+          x
+        </button>
       </nav>
     </header>
     <article>{children}</article>
--- components/Windows.tsx
+++ components/Windows.tsx
@@ -5,14 +5,19 @@ import { useContext } from 'react';
 import { Window } from './Window';
 
 export const Windows: FC = () => {
-  const { apps } = useContext(AppsContext);
+  const { apps, updateApps } = useContext(AppsContext);
 
   return (
     <ol className={styles.windows}>
       {apps
         .filter(({ running, minimized }) => running && !minimized)
         .map(({ component: App, id, name }) => (
-          <Window key={id} name={name}>
+          <Window
+            key={id}
+            name={name}
+            onMinimize={() => updateApps({ update: { minimized: true }, id })}
+            onClose={() => updateApps({ update: { running: false }, id })} // TODO: Need to reset states on close?
+          >
             <App />
           </Window>
         ))}
