e38ac4792804757e7a0074e6618ffbe1c1fd6911
Dustin Brett <dustin@voxter.ca>
2020-08-27 00:01:02 -0700

    1 liner I guess

--- components/Games/Doom.tsx
+++ components/Games/Doom.tsx
@@ -4,11 +4,6 @@ import type { FC } from 'react';
 import App from '@/contexts/App';
 import Dos from '@/components/Dos';
 
-const Doom: FC = () => (
-  <Dos
-    url="/games/doom.ZIP"
-    args={['-c', 'DOOM.EXE']}
-  />
-);
+const Doom: FC = () => <Dos url="/games/doom.ZIP" args={['-c', 'DOOM.EXE']} />;
 
 export default new App(Doom, <DosIcon />, 'doom', 'Doom');
