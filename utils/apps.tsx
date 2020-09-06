ce4dc84f97e0d7ef4cbfda49a626c1753619219f
Dustin Brett <dustin@voxter.ca>
2020-09-05 17:56:58 -0700

    Tweak

--- utils/apps.tsx
+++ utils/apps.tsx
@@ -3,7 +3,10 @@ import type { ReactElement } from 'react';
 import { DosAppLoader } from '@/components/Apps/Dos';
 import { getFileExtension } from '@/utils/files';
 
-const getAppComponentByName = (name: string): ReactElement | undefined => {
+const getAppComponentByName = (
+  name: string,
+  searchParams: URLSearchParams
+): ReactElement | undefined => {
   switch (name) {
     case 'dos':
       return <DosAppLoader />;
@@ -26,6 +29,6 @@ export const getAppComponent = (url: string): ReactElement | undefined => {
   const { hash, pathname, searchParams } = new URL(url);
 
   return hash
-    ? getAppComponentByName(hash.replace('#', ''))
+    ? getAppComponentByName(hash.replace('#', ''), searchParams)
     : getAppComponentByFileType(getFileExtension(pathname), searchParams);
 };
