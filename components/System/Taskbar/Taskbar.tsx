90ed62c9a619f57ef5b0dd498a768d54e82f2c2e
Dustin Brett <dustin@voxter.ca>
2020-09-05 21:38:25 -0700

    Tweaks

--- components/System/Taskbar/Taskbar.tsx
+++ components/System/Taskbar/Taskbar.tsx
@@ -1,40 +1,40 @@
 import styles from '@/styles/System/Taskbar.module.scss';
 
-import type { FC } from 'react';
+import { FC, useContext } from 'react';
 
 import { AppsContext } from '@/contexts/Apps';
 import { Clock } from '@/components/System/Taskbar/Clock';
 import { TaskbarEntry } from '@/components/System/Taskbar/TaskbarEntry';
 import { sortByLastRunning } from '@/utils/utils';
 
-export const Taskbar: FC = () => (
-  <AppsContext.Consumer>
-    {({ apps, focus, minimize }) => (
-      <nav className={styles.taskbar}>
-        <ol>
-          {apps
-            ?.sort(sortByLastRunning)
-            .map(({ id, icon, minimized, name, foreground, stackOrder }) => (
-              <TaskbarEntry
-                key={id}
-                foreground={foreground}
-                icon={icon}
-                name={name}
-                onClick={() => {
-                  if (minimized) {
-                    minimize?.(id, false);
-                  } else {
-                    const [foregroundApp] = stackOrder;
+export const Taskbar: FC = () => {
+  const { apps, focus, minimize } = useContext(AppsContext);
 
-                    foregroundApp === id ? minimize?.(id) : focus?.(id);
-                  }
-                }}
-                tabIndex={0}
-              />
-            ))}
-        </ol>
-        <Clock />
-      </nav>
-    )}
-  </AppsContext.Consumer>
-);
+  return (
+    <nav className={styles.taskbar}>
+      <ol>
+        {apps
+          ?.sort(sortByLastRunning)
+          .map(({ id, icon, minimized, name, foreground, stackOrder }) => (
+            <TaskbarEntry
+              key={id}
+              foreground={foreground}
+              icon={icon}
+              name={name}
+              onClick={() => {
+                if (minimized) {
+                  minimize?.(id, false);
+                } else {
+                  const [foregroundApp] = stackOrder;
+
+                  foregroundApp === id ? minimize?.(id) : focus?.(id);
+                }
+              }}
+              tabIndex={0}
+            />
+          ))}
+      </ol>
+      <Clock />
+    </nav>
+  );
+};
