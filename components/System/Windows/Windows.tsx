f61996b1686ea20f76ab77f2f44995d007d0d514
Dustin Brett <dustin@voxter.ca>
2020-09-08 20:34:14 -0700

    Window motion

--- components/System/Windows/Windows.tsx
+++ components/System/Windows/Windows.tsx
@@ -4,8 +4,28 @@ import { useContext, useEffect, useState } from 'react';
 import { AppsContext } from '@/contexts/Apps';
 import { sortByLastRunning } from '@/utils/utils';
 import dynamic from 'next/dynamic';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Window = dynamic(import('@/components/System/Windows/Window'));
+
+const windowMotionSettings = {
+  initial: { opacity: 0, y: 100 },
+  animate: { opacity: 1, y: 0 },
+  transition: {
+    y: {
+      damping: 15,
+      stiffness: 800,
+      type: 'spring'
+    }
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.3
+    },
+    y: 100
+  }
+};
 
 export const Windows: FC = () => {
   const { apps, close, focus, minimize, position, size } = useContext(
@@ -28,62 +48,70 @@ export const Windows: FC = () => {
     // W3C: Section lacks heading. Consider using h2-h6 elements to add identifying headings to all sections.
     // https://www.w3.org/wiki/HTML/Usage/Headings/Missing
     <section style={windowMargins}>
-      {apps
-        ?.sort(sortByLastRunning)
-        .map(
-          (
-            {
-              loader: { loader: App, loadedAppOptions },
-              id,
-              icon,
-              name,
-              windowed,
-              minimized,
-              foreground,
-              lockAspectRatio,
-              hideScrollbars,
-              stackOrder,
-              height,
-              width,
-              x,
-              y
-            },
-            index
-          ) => {
-            const cascadeSpacing = index * 20 || 0,
-              appOptions = {
-                onMinimize: () => minimize?.(id),
-                onClose: () => close?.(id, stackOrder),
-                onFocus: () => focus?.(id),
-                onBlur: () => focus?.(id, false),
-                updatePosition: position?.(id),
-                updateSize: size?.(id),
-                zIndex: 1750 + (apps.length - (stackOrder.indexOf(id) + 1)), // TODO: Still valid logic?
-                foreground,
+      <AnimatePresence>
+        {apps
+          ?.sort(sortByLastRunning)
+          .map(
+            (
+              {
+                loader: { loader: App, loadedAppOptions },
+                id,
+                icon,
+                name,
+                windowed,
                 minimized,
+                foreground,
+                lockAspectRatio,
+                hideScrollbars,
+                stackOrder,
                 height,
                 width,
-                x: x || cascadeSpacing,
-                y: y || cascadeSpacing
-              };
-
-            return windowed ? (
-              <Window
-                key={id}
-                icon={icon}
-                name={name}
-                lockAspectRatio={lockAspectRatio}
-                hideScrollbars={hideScrollbars}
-                updateSize={size?.(id)}
-                {...appOptions}
-              >
-                <App {...loadedAppOptions} />
-              </Window>
-            ) : (
-              <App key={id} {...appOptions} {...loadedAppOptions} />
-            );
-          }
-        )}
+                x,
+                y
+              },
+              index
+            ) => {
+              const cascadeSpacing = index * 20 || 0,
+                appOptions = {
+                  onMinimize: () => minimize?.(id),
+                  onClose: () => close?.(id, stackOrder),
+                  onFocus: () => focus?.(id),
+                  onBlur: () => focus?.(id, false),
+                  updatePosition: position?.(id),
+                  updateSize: size?.(id),
+                  zIndex: 1750 + (apps.length - (stackOrder.indexOf(id) + 1)), // TODO: Still valid logic?
+                  foreground,
+                  minimized,
+                  height,
+                  width,
+                  x: x || cascadeSpacing,
+                  y: y || cascadeSpacing
+                };
+              
+              return (
+                <motion.div
+                  key={id}
+                  {...windowMotionSettings}
+                >
+                  {windowed ? (
+                    <Window
+                      icon={icon}
+                      name={name}
+                      lockAspectRatio={lockAspectRatio}
+                      hideScrollbars={hideScrollbars}
+                      updateSize={size?.(id)}
+                      {...appOptions}
+                    >
+                      <App {...loadedAppOptions} />
+                    </Window>
+                  ) : (
+                    <App key={id} {...appOptions} {...loadedAppOptions} />
+                  )}
+                </motion.div>
+              );
+            }
+          )}
+      </AnimatePresence>
     </section>
   );
 };
