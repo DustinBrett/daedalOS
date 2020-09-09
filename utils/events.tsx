f6d36fd1a5e4cc8b4387b408a99cc26d105ebc3d
Dustin Brett <dustin@voxter.ca>
2020-09-08 22:33:16 -0700

    Tweak

--- utils/events.tsx
+++ utils/events.tsx
@@ -10,23 +10,23 @@ export class ClickHandler {
     singleClick,
     doubleClick
   }: {
-    singleClick?: () => void;
-    doubleClick?: () => void;
+    singleClick?: (event: React.MouseEvent<Element, MouseEvent>) => void;
+    doubleClick?: (event: React.MouseEvent<Element, MouseEvent>) => void;
   }) {
     this.singleClick = singleClick;
     this.doubleClick = doubleClick;
   }
 
-  clickHandler = (): void => {
+  clickHandler = (event: React.MouseEvent<Element, MouseEvent>): void => {
     if (!this.clickTimer) {
       this.clickTimer = setTimeout(() => {
         this.clickTimer = undefined;
-        this.singleClick?.();
+        this.singleClick?.(event);
       }, CLICK_DELAY_IN_MILLISECONDS);
     } else {
       clearTimeout(this.clickTimer);
       this.clickTimer = undefined;
-      this.doubleClick?.();
+      this.doubleClick?.(event);
     }
   };
 }
