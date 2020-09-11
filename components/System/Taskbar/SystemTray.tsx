641b7cd34e74fe37227d0089577c6ddf79130b7f
Dustin Brett <dustin@voxter.ca>
2020-09-10 23:13:26 -0700

    Battery

--- components/System/Taskbar/SystemTray.tsx
+++ components/System/Taskbar/SystemTray.tsx
@@ -2,16 +2,38 @@ import styles from '@/styles/System/SystemTray.module.scss';
 
 import type { FC } from 'react';
 
+import { useEffect, useState } from 'react';
 import { useBattery } from 'react-use';
+import { BatteryState } from 'react-use/lib/useBattery';
+
+// TODO: Extract out this battery element from the system tray which will contain network/volume some day too
+
+type UseBatteryState = BatteryState & { isSupported: true; fetched: true };
 
 export const SystemTray: FC = () => {
-  const batteryState = useBattery();
+  const batteryState = useBattery(),
+    [batteryLevel, setBatteryLevel] = useState(-1),
+    [batteryCharging, setBatteryCharging] = useState(false);
+
+  useEffect(() => {
+    const {
+      isSupported,
+      fetched,
+      level,
+      charging
+    }: UseBatteryState = (batteryState || {}) as UseBatteryState;
+
+    if (isSupported && fetched) {
+      setBatteryLevel(level);
+      setBatteryCharging(charging);
+    }
+  }, [batteryState]);
 
   return (
     <nav className={styles.tray}>
       <ol>
-        <li title={`Charging: ${batteryState?.charging ? 'yes' : 'no'}`}>
-          {batteryState?.level * 100}%
+        <li title={`Charging: ${batteryCharging ? 'yes' : 'no'}`}>
+          {batteryLevel >= 0 ? batteryLevel * 100 : '?'}%
         </li>
       </ol>
     </nav>
