49f45b5473fb7f4d94c3eaa17d12494f736d9374
Dustin Brett <dustin@voxter.ca>
2020-10-09 20:02:55 -0700

    Fix and notes

--- components/System/Taskbar/StartMenu.tsx
+++ components/System/Taskbar/StartMenu.tsx
@@ -3,6 +3,12 @@ import styles from '@/styles/System/Taskbar/StartMenu.module.scss';
 import { faWindows } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// TODO:
+// - The menu can be <ol>.
+// - CSS positionined directly above button
+// - 0 height by default
+// - animate to 100% height
+
 const StartMenu: React.FC = () => (
   <nav className={styles.start}>
     <FontAwesomeIcon icon={faWindows} />
