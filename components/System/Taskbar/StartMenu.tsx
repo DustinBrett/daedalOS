70129fb6a984d7c04c8e2de072c3ce6d808cca0f
Dustin Brett <dustin@voxter.ca>
2020-10-09 20:57:07 -0700

    tweaks and notes

--- components/System/Taskbar/StartMenu.tsx
+++ components/System/Taskbar/StartMenu.tsx
@@ -8,9 +8,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // - CSS positionined directly above button
 // - 0 height by default
 // - animate to 100% height
+// - On click the bg color gets lighter
+// - Stays lighter until menu goes away
+// - 375px width
+// - hover on start menu has a circle gradient from cursor for 15-20px
+// - hover lights up borders of buttons and bg of items
+// - START | All Apps | Dustin Brett | Documents | Power
+// - Highlight color for icon, text color and left border for buttons, 4px
+// - Title on hover is "Start"
+// - 1px border on right side of button menu
 
 const StartMenu: React.FC = () => (
-  <nav className={styles.start}>
+  <nav className={styles.start} title="Start">
     <FontAwesomeIcon icon={faWindows} />
   </nav>
 );
