cc72d4b9d9876e39aaeafdb7f5e8e10383956719
Dustin Brett <dustin@voxter.ca>
2020-10-16 22:25:40 -0700

    Moved window stuff

---
+++ components/System/WindowManager/ProcessWindow.tsx
@@ -0,0 +1,154 @@
+import styles from '@/styles/System/WindowManager/WindowManager.module.scss';
+
+import type { Process } from '@/utils/process';
+
+import dynamic from 'next/dynamic';
+import {
+  baseZindex,
+  foregroundZindex,
+  windowsZindexLevel,
+  zindexLevelSize
+} from '@/utils/constants';
+import {
+  focusNextVisibleWindow,
+  getMaxDimensions
+} from '@/utils/windowmanager';
+import { motion } from 'framer-motion';
+import { ProcessContext } from '@/contexts/ProcessManager';
+import { SessionContext } from '@/contexts/SessionManager';
+import { useCallback, useContext } from 'react';
+import { windowMotionSettings } from '@/utils/motions';
+
+const Window = dynamic(import('@/components/System/WindowManager/Window'));
+
+const windowZindex = baseZindex + windowsZindexLevel * zindexLevelSize;
+
+const ProcessWindow: React.FC<Process> = ({
+  loader: {
+    loader: App,
+    loadedAppOptions,
+    loaderOptions: { width: defaultWidth, height: defaultHeight }
+  },
+  bgColor,
+  height: initialHeight,
+  icon,
+  id,
+  launchElement,
+  lockAspectRatio,
+  maximized,
+  minimized,
+  name,
+  taskbarElement,
+  width: initialWidth,
+  windowed,
+  x,
+  y
+}) => {
+  const {
+    foreground,
+    getState,
+    saveState,
+    session: { foregroundId, stackOrder }
+  } = useContext(SessionContext);
+  const {
+    processes,
+    close,
+    maximize,
+    minimize,
+    position,
+    restore,
+    size
+  } = useContext(ProcessContext);
+  const { x: previousX = 0, y: previousY = 0 } = getState({
+    id
+  });
+  const { height, width } = getMaxDimensions(
+    initialWidth,
+    initialHeight,
+    defaultWidth,
+    defaultHeight,
+    lockAspectRatio
+  );
+  const windowOptions = {
+    onMinimize: useCallback(() => {
+      // TODO: Allow restoring maximized. Fix max restore.
+      // if (maximized) restore(id, 'maximized');
+      minimize(id);
+      focusNextVisibleWindow(stackOrder, processes, foreground);
+    }, [id, processes, stackOrder]),
+    onMaximize: useCallback(
+      () => (maximized ? restore(id, 'maximized') : maximize(id)),
+      [id, maximized]
+    ),
+    onClose: useCallback(() => {
+      saveState({
+        id,
+        height,
+        width,
+        x,
+        y
+      });
+      close(id);
+      focusNextVisibleWindow(stackOrder, processes, foreground);
+    }, [height, id, processes, stackOrder, width, x, y]),
+    onFocus: useCallback(() => foreground(id), [id]),
+    onBlur: useCallback(
+      (event: React.FocusEvent) => {
+        if (event.relatedTarget !== taskbarElement) {
+          foreground('');
+        }
+      },
+      [taskbarElement]
+    ),
+    updatePosition: useCallback((event, data) => position(id)(event, data), [
+      id
+    ]),
+    zIndex: windowZindex + stackOrder.slice().reverse().indexOf(id),
+    maximized,
+    minimized,
+    id,
+    height,
+    width
+  };
+
+  return (
+    <motion.article
+      key={id}
+      className={styles.animatedWindows}
+      style={{
+        zIndex: foregroundId === id ? foregroundZindex : windowOptions.zIndex,
+        height,
+        width
+      }}
+      {...windowMotionSettings({
+        initialX: previousX,
+        initialY: previousY,
+        animation:
+          (minimized && 'minimized') || (maximized && 'maximized') || 'start',
+        height,
+        width,
+        x,
+        y,
+        taskbarElement,
+        launchElement
+      })}
+    >
+      {windowed ? (
+        <Window
+          icon={icon}
+          name={name}
+          bgColor={bgColor}
+          lockAspectRatio={lockAspectRatio}
+          updateSize={size(id)}
+          {...windowOptions}
+        >
+          <App {...loadedAppOptions} {...windowOptions} />
+        </Window>
+      ) : (
+        <App key={id} {...loadedAppOptions} {...windowOptions} />
+      )}
+    </motion.article>
+  );
+};
+
+export default ProcessWindow;
--- components/System/WindowManager/WindowManager.tsx
+++ components/System/WindowManager/WindowManager.tsx
@@ -1,160 +1,24 @@
 import styles from '@/styles/System/WindowManager/WindowManager.module.scss';
 
+import { AnimatePresence } from 'framer-motion';
+
 import dynamic from 'next/dynamic';
-import { AnimatePresence, motion } from 'framer-motion';
-import {
-  baseZindex,
-  foregroundZindex,
-  windowsZindexLevel,
-  zindexLevelSize
-} from '@/utils/constants';
-import {
-  focusNextVisibleWindow,
-  getMaxDimensions
-} from '@/utils/windowmanager';
 import { ProcessContext } from '@/contexts/ProcessManager';
-import { SessionContext } from '@/contexts/SessionManager';
 import { useContext } from 'react';
-import { windowMotionSettings } from '@/utils/motions';
 
-const Window = dynamic(import('@/components/System/WindowManager/Window'));
-
-// TODO: I need to memo some of this stuff
+const ProcessWindow = dynamic(
+  import('@/components/System/WindowManager/ProcessWindow')
+);
 
 const WindowManager: React.FC = () => {
-  const {
-    foreground,
-    getState,
-    saveState,
-    session: { foregroundId, stackOrder }
-  } = useContext(SessionContext);
-  const {
-    processes,
-    close,
-    maximize,
-    minimize,
-    position,
-    restore,
-    size
-  } = useContext(ProcessContext);
+  const { processes } = useContext(ProcessContext);
 
   return (
     <article className={styles.windows}>
       <AnimatePresence>
-        {processes.map(
-          ({
-            loader: {
-              loader: App,
-              loadedAppOptions,
-              loaderOptions: { width: defaultWidth, height: defaultHeight }
-            },
-            id,
-            icon,
-            name,
-            bgColor,
-            windowed,
-            maximized,
-            minimized,
-            lockAspectRatio,
-            height: initialHeight,
-            width: initialWidth,
-            x,
-            y,
-            taskbarElement,
-            launchElement
-          }) => {
-            const { x: previousX = 0, y: previousY = 0 } = getState({
-              id
-            });
-            const windowZindex =
-              baseZindex + windowsZindexLevel * zindexLevelSize;
-            const { height, width } = getMaxDimensions(
-              initialWidth,
-              initialHeight,
-              defaultWidth,
-              defaultHeight,
-              lockAspectRatio
-            );
-            const windowOptions = {
-              onMinimize: () => {
-                // TODO: Allow restoring maximized. Fix max restore.
-                // if (maximized) restore(id, 'maximized');
-                minimize(id);
-                focusNextVisibleWindow(stackOrder, processes, foreground);
-              },
-              onMaximize: () =>
-                maximized ? restore(id, 'maximized') : maximize(id),
-              onClose: () => {
-                saveState({
-                  id,
-                  height,
-                  width,
-                  x,
-                  y
-                });
-                close(id);
-                focusNextVisibleWindow(stackOrder, processes, foreground);
-              },
-              onFocus: () => foreground(id),
-              onBlur: (event: React.FocusEvent) => {
-                if (event.relatedTarget !== taskbarElement) {
-                  foreground('');
-                }
-              },
-              updatePosition: position(id),
-              zIndex: windowZindex + stackOrder.slice().reverse().indexOf(id),
-              maximized,
-              minimized,
-              id,
-              height,
-              width
-            };
-
-            return (
-              <motion.article
-                key={id}
-                className={styles.animatedWindows}
-                style={{
-                  zIndex:
-                    foregroundId === id
-                      ? foregroundZindex
-                      : windowOptions.zIndex,
-                  height,
-                  width
-                }}
-                {...windowMotionSettings({
-                  initialX: previousX,
-                  initialY: previousY,
-                  animation:
-                    (minimized && 'minimized') ||
-                    (maximized && 'maximized') ||
-                    'start',
-                  height,
-                  width,
-                  x,
-                  y,
-                  taskbarElement,
-                  launchElement
-                })}
-              >
-                {windowed ? (
-                  <Window
-                    icon={icon}
-                    name={name}
-                    bgColor={bgColor}
-                    lockAspectRatio={lockAspectRatio}
-                    updateSize={size(id)}
-                    {...windowOptions}
-                  >
-                    <App {...loadedAppOptions} {...windowOptions} />
-                  </Window>
-                ) : (
-                  <App key={id} {...loadedAppOptions} {...windowOptions} />
-                )}
-              </motion.article>
-            );
-          }
-        )}
+        {processes.map((process) => (
+          <ProcessWindow key={process.id} {...process} />
+        ))}
       </AnimatePresence>
     </article>
   );
