dfa2397711eca8031096a14200547bf820d0a669
Dustin Brett <dustin@voxter.ca>
2020-10-16 22:13:19 -0700

    Better ani

--- components/System/Taskbar/TaskbarEntries.tsx
+++ components/System/Taskbar/TaskbarEntries.tsx
@@ -3,31 +3,32 @@ import styles from '@/styles/System/Taskbar/TaskbarEntries.module.scss';
 import dynamic from 'next/dynamic';
 import { AnimatePresence } from 'framer-motion';
 import { ProcessContext } from '@/contexts/ProcessManager';
+import { useContext } from 'react';
 
 const TaskbarEntry = dynamic(
   import('@/components/System/Taskbar/TaskbarEntry')
 );
 
-const TaskbarEntries: React.FC = () => (
-  <nav>
-    <ol className={styles.taskbarEntries}>
-      <AnimatePresence>
-        <ProcessContext.Consumer>
-          {({ processes }) =>
-            processes.map(({ icon, id, minimized, name }) => (
-              <TaskbarEntry
-                key={id}
-                icon={icon}
-                id={id}
-                minimized={minimized}
-                name={name}
-              />
-            ))
-          }
-        </ProcessContext.Consumer>
-      </AnimatePresence>
-    </ol>
-  </nav>
-);
+const TaskbarEntries: React.FC = () => {
+  const { processes } = useContext(ProcessContext);
+
+  return (
+    <nav>
+      <ol className={styles.taskbarEntries}>
+        <AnimatePresence>
+          {processes.map(({ icon, id, minimized, name }) => (
+            <TaskbarEntry
+              key={id}
+              icon={icon}
+              id={id}
+              minimized={minimized}
+              name={name}
+            />
+          ))}
+        </AnimatePresence>
+      </ol>
+    </nav>
+  );
+};
 
 export default TaskbarEntries;
