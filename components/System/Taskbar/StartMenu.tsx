abfbd9fb8dc47af78841bba267cc81c6ecdc36af
Dustin Brett <dustin@voxter.ca>
2020-10-19 23:42:50 -0700

    Tweak

--- components/System/Taskbar/StartMenu.tsx
+++ components/System/Taskbar/StartMenu.tsx
@@ -7,14 +7,6 @@ import { faWindows } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 
-// TODO
-// - On click the button bg color gets lighter
-// - Stays lighter until menu goes away
-// - hover on start menu has a circle gradient from cursor for 15-20px
-// - hover lights up borders of buttons and bg of items
-// - Highlight color for icon, text color and left border (4px) for buttons
-// - 1px border on right side of button menu
-
 const StartMenu: React.FC = () => {
   const [showMenu, setShowMenu] = useState(false);
 
