e74c5e01f872ae72d66ce47f569604817b923cd5
Dustin Brett <dustin@voxter.ca>
2020-10-20 21:25:22 -0700

    Another button

--- components/System/Taskbar/StartMenu.tsx
+++ components/System/Taskbar/StartMenu.tsx
@@ -2,6 +2,7 @@ import styles from '@/styles/System/Taskbar/StartMenu.module.scss';
 
 import FileManager from '@/components/System/FileManager/FileManager';
 import MenuView from '@/components/System/FileManager/MenuView';
+import { faFile } from '@fortawesome/free-regular-svg-icons';
 import { faBars, faList } from '@fortawesome/free-solid-svg-icons';
 import { faWindows } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -20,6 +21,9 @@ const StartMenu: React.FC = () => {
             </li>
             <li className={styles.buttonSelected}>
               <FontAwesomeIcon icon={faList} />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faFile} />
             </li>
           </ol>
           <FileManager
