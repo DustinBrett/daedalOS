2c8acc62abbed3d30a2a8add8ae0d242345ccbee
Dustin Brett <dustin@voxter.ca>
2020-10-24 22:57:45 -0700

    Tweaks

--- components/System/Icon.tsx
+++ components/System/Icon.tsx
@@ -1,39 +1,39 @@
-import DoomImage from '@/public/icons/games/doom.png';
-import DosImage from '@/public/icons/programs/dos.png';
-import ExplorerImage from '@/public/icons/programs/explorer.png';
-import JsFileImage from '@/public/icons/files/js.svg';
-import KeenImage from '@/public/icons/games/keen.png';
-import UnknownFileImage from '@/public/icons/files/unknown.svg';
-import WinampImage from '@/public/icons/programs/winamp.png';
+import DoomImg from '@/public/icons/games/doom.png';
+import DosImg from '@/public/icons/programs/dos.png';
+import ExplorerImg from '@/public/icons/programs/explorer.png';
+import JsFileImg from '@/public/icons/files/js.svg';
+import KeenImg from '@/public/icons/games/keen.png';
+import UnknownFileImg from '@/public/icons/files/unknown.svg';
+import WinampImg from '@/public/icons/programs/winamp.png';
 
 import Img from 'react-optimized-image';
 import { basename } from 'path';
 import { memo } from 'react';
 
-type IconProps = { height: number, src: string, width: number };
+type IconProps = { height: number; src: string; width: number };
 
 const Icon: React.FC<IconProps> = ({ src, width, height }) => {
-  const dimensions = { width, height };
+  const size = { width, height };
 
   switch (src) {
     case '/icons/files/js.svg':
-      return <Img url {...dimensions} src={JsFileImage} alt="JavaScript" />;
+      return <Img url {...size} src={JsFileImg} alt="JavaScript" />;
     case '/icons/files/unknown.svg':
-      return <Img url {...dimensions} src={UnknownFileImage} alt="Unknown" />;
+      return <Img url {...size} src={UnknownFileImg} alt="Unknown" />;
     case '/icons/programs/dos.png':
-      return <Img url {...dimensions} src={DosImage} alt="DOS" />;
+      return <Img url {...size} src={DosImg} alt="DOS" />;
 
     case '/icons/games/keen.png':
-      return <Img {...dimensions} src={KeenImage} alt="Commander Keen" type="icon" />;
+      return <Img {...size} src={KeenImg} alt="Commander Keen" type="icon" />;
     case '/icons/games/doom.png':
-      return <Img {...dimensions} src={DoomImage} alt="Doom" type="icon" />;
+      return <Img {...size} src={DoomImg} alt="Doom" type="icon" />;
     case '/icons/programs/explorer.png':
-      return <Img {...dimensions} src={ExplorerImage} alt="Explorer" type="icon" />;
+      return <Img {...size} src={ExplorerImg} alt="Explorer" type="icon" />;
     case '/icons/programs/winamp.png':
-      return <Img {...dimensions} src={WinampImage} alt="Winamp" type="icon" />;
+      return <Img {...size} src={WinampImg} alt="Winamp" type="icon" />;
 
     default:
-      return <img {...dimensions} src={src} alt={basename(src)} />;
+      return <img {...size} src={src} alt={basename(src)} />;
   }
 };
 
