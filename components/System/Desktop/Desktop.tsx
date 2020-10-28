532d923ef6440540f3b50e6683d35a92996d3a18
Dustin Brett <dustin@voxter.ca>
2020-10-28 16:05:38 -0700

    Simpler

--- components/System/Desktop/Desktop.tsx
+++ components/System/Desktop/Desktop.tsx
@@ -1,16 +1,13 @@
 import styles from '@/styles/System/Desktop/Desktop.module.scss';
 
-import type { WallpaperEffect } from '@/types/components/System/Desktop/Wallpaper';
-
 import { renderWallpaperEffect } from '@/components/System/Desktop/Wallpaper';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 const Desktop: React.FC = ({ children }) => {
   const desktopRef = useRef<HTMLElement>(null);
-  const [wallpaperEffect, setWallpaperEffect] = useState<WallpaperEffect>();
 
   useEffect(() => {
-    setWallpaperEffect(renderWallpaperEffect(desktopRef));
+    const wallpaperEffect = renderWallpaperEffect(desktopRef);
 
     return () => {
       wallpaperEffect?.destroy();
