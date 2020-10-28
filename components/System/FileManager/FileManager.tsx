66158b908283d490bb97f5ebdb9eb77ec0a66793
Dustin Brett <dustin@voxter.ca>
2020-10-28 16:18:00 -0700

    Forgot to get rid of this guy

--- components/System/FileManager/FileManager.tsx
+++ components/System/FileManager/FileManager.tsx
@@ -21,7 +21,7 @@ const FileManager: React.FC<DirectoryType> = ({
   const [cwd, cd] = useState(directoryPath);
   const [entries, setEntries] = useState<DirectoryEntry[]>([]);
   const fs = useContext(FileContext);
-  const { load, open, restore } = useContext(ProcessContext);
+  const { load, open } = useContext(ProcessContext);
   const { foreground, getState } = useContext(SessionContext);
   const fileDropHandler = useFileDrop(async (dragEvent, file) => {
     load(file, getState({ name: file.name }), dragEvent.target);
