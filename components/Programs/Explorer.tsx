41d2cf8fce26401c136d83c20ac683a90dc064ad
Dustin Brett <dustin@voxter.ca>
2020-11-09 22:47:32 -0800

    Focus people

--- components/Programs/Explorer.tsx
+++ components/Programs/Explorer.tsx
@@ -5,24 +5,27 @@ import type { AppComponent } from '@/types/utils/programs';
 import FileManager from '@/components/System/FileManager/FileManager';
 import ListView from '@/components/System/FileManager/ListView';
 import { basename } from 'path';
+import { focusClosestFocusableElementFromRef } from '@/utils/elements';
 import { ProcessContext } from '@/contexts/ProcessManager';
 import { ROOT_DIRECTORY } from '@/utils/constants';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 
 const Explorer: React.FC<AppComponent> = ({
   id = '',
   file: { url = ROOT_DIRECTORY } = {}
 }) => {
+  const explorerRef = useRef<HTMLElement>(null);
   const { title } = useContext(ProcessContext);
 
   return (
-    <article className={styles.explorer}>
+    <article className={styles.explorer} ref={explorerRef}>
       <FileManager
         path={url}
         render={ListView}
         onChange={(cwd) => {
           if (cwd) {
             title(id, cwd === ROOT_DIRECTORY ? 'home' : basename(cwd));
+            focusClosestFocusableElementFromRef(explorerRef)();
           }
         }}
         details
