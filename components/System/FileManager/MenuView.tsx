dbfbfdef0bc86d1655f473244a21a2a74a4a5dda
Dustin Brett <dustin@voxter.ca>
2020-11-24 13:44:53 -0800

    Load faster

--- components/System/FileManager/MenuView.tsx
+++ components/System/FileManager/MenuView.tsx
@@ -3,28 +3,23 @@ import styles from '@/styles/System/FileManager/MenuView.module.scss';
 import type { DirectoryView } from '@/types/components/System/FileManager/FileManager';
 
 import Icon from '@/components/System/Icon';
-import { AnimatePresence, motion } from 'framer-motion';
-import { startMenuEntriesMotionSettings } from '@/utils/motions';
 
 const DirectoryMenu: React.FC<DirectoryView> = ({ entries, onDoubleClick }) => {
   return (
     <ol className={styles.menuView}>
-      <AnimatePresence>
-        {entries.map(({ icon, name, path, url }) => (
-          <motion.li
-            key={path}
-            onClick={onDoubleClick({ path, url, icon, name })}
-            {...startMenuEntriesMotionSettings}
-          >
-            <figure>
-              <div className={styles.menuIcon}>
-                <Icon src={icon} height={32} width={32} />
-              </div>
-              <figcaption>{name}</figcaption>
-            </figure>
-          </motion.li>
-        ))}
-      </AnimatePresence>
+      {entries.map(({ icon, name, path, url }) => (
+        <li
+          key={path}
+          onClick={onDoubleClick({ path, url, icon, name })}
+        >
+          <figure>
+            <div className={styles.menuIcon}>
+              <Icon src={icon} height={32} width={32} />
+            </div>
+            <figcaption>{name}</figcaption>
+          </figure>
+        </li>
+      ))}
     </ol>
   );
 };
