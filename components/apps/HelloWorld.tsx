b37200f58554dc76568af0fa7149538e049b1ab5
Dustin Brett <dustinbrett@gmail.com>
2021-01-16 22:49:16 -0800

    Extracted Hello World app

---
+++ components/apps/HelloWorld.tsx
@@ -0,0 +1,6 @@
+import type { FC } from 'react';
+import Title from 'styles/generic/Title';
+
+const HelloWorld: FC = () => <Title>Hello, world!</Title>;
+
+export default HelloWorld;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
+import HelloWorld from 'components/apps/HelloWorld';
 import type { ReactElement } from 'react';
-import Title from 'styles/generic/Title';
 
 export default function Home(): ReactElement {
-  return <Title>Hello, world!</Title>;
+  return <HelloWorld />;
 }
