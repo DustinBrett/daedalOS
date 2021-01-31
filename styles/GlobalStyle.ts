cb640e2067e2b190845247a3d12dd043fff2cf36
Dustin Brett <dustinbrett@gmail.com>
2021-01-30 23:06:11 -0800

    More global css resetting

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,14 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    margin: 0;
-    padding: 0;
-  }
-
   * {
+    border: 0;
     box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
   }
 `;
 
