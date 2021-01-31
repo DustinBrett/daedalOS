1a016065f0d6f773d5fcaa5f519efe229f2f4e7f
Dustin Brett <dustinbrett@gmail.com>
2021-01-30 22:00:25 -0800

    Added RenderProcess component & dynamic Window

--- components/system/ProcessLoader.tsx
+++ components/system/ProcessLoader.tsx
@@ -1,19 +1,25 @@
-import Window from 'components/system/Window';
 import { ProcessConsumer } from 'contexts/process';
+import dynamic from 'next/dynamic';
 import type { FC } from 'react';
+import type { Process } from 'types/contexts/process';
+
+const Window = dynamic(() => import('components/system/Window'));
+
+const RenderProcess: FC<Process> = ({ Component, hasWindow }) =>
+  hasWindow ? (
+    <Window>
+      <Component />
+    </Window>
+  ) : (
+    <Component />
+  );
 
 const ProcessLoader: FC = () => (
   <ProcessConsumer>
     {({ processes }) =>
-      Object.entries(processes).map(([id, { Component, hasWindow }]) =>
-        hasWindow ? (
-          <Window key={id}>
-            <Component />
-          </Window>
-        ) : (
-          <Component key={id} />
-        )
-      )
+      Object.entries(processes).map(([id, process]) => (
+        <RenderProcess key={id} {...process} />
+      ))
     }
   </ProcessConsumer>
 );
