05ca5325c1cbb2a1f852b9f715f18103817b18ab
Dustin Brett <dustinbrett@gmail.com>
2021-02-13 23:01:16 -0800

    Moved metadata into theme context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,12 @@ import { SessionProvider } from 'contexts/session';
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps): React.ReactElement => (
-  <>
-    <Metadata />
-    <SessionProvider>
-      <StyledApp>
-        <Component {...pageProps} />
-      </StyledApp>
-    </SessionProvider>
-  </>
+  <SessionProvider>
+    <StyledApp>
+      <Metadata />
+      <Component {...pageProps} />
+    </StyledApp>
+  </SessionProvider>
 );
 
 export default App;
