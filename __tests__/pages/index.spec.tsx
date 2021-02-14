c9083ba5babfe1b0bbf0db43c6a8028f542221a3
Dustin Brett <dustinbrett@gmail.com>
2021-02-13 22:58:21 -0800

    Fixed/Refactored test to best practices

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -1,13 +1,13 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import StyledApp from 'components/pages/StyledApp';
 import Index from 'pages/index';
 
-test('renders index page', () => {
-  const { getByText } = render(
+test('renders main role', () => {
+  render(
     <StyledApp>
       <Index />
     </StyledApp>
   );
 
-  expect(getByText('Hello, world!')).toBeInTheDocument();
+  expect(screen.getByRole('main')).toBeInTheDocument();
 });
