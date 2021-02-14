eff090cfcfe74ffd5524f0fef3b235086c7881be
Dustin Brett <dustinbrett@gmail.com>
2021-02-13 22:23:36 -0800

    Don't use width for StyledTaskbarEntries

--- styles/components/system/Taskbar/StyledTaskbarEntries.ts
+++ styles/components/system/Taskbar/StyledTaskbarEntries.ts
@@ -6,7 +6,6 @@ const StyledTaskbarEntries = styled.ol`
   left: ${({ theme }) => theme.sizes.startButton.width};
   position: absolute;
   right: ${({ theme }) => theme.sizes.clock.width};
-  width: 100%;
 `;
 
 export default StyledTaskbarEntries;
