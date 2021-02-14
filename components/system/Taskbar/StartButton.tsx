b60a1a673ca1d7cdc35c7b7cf2e2932c6b40479d
Dustin Brett <dustinbrett@gmail.com>
2021-02-13 23:29:08 -0800

    Added Start tooltip to start button

--- components/system/Taskbar/StartButton.tsx
+++ components/system/Taskbar/StartButton.tsx
@@ -1,7 +1,7 @@
 import StyledStartButton from 'styles/components/system/Taskbar/StyledStartButton';
 
 const StartButton = (): JSX.Element => (
-  <StyledStartButton type="button">X</StyledStartButton>
+  <StyledStartButton type="button" title="Start">X</StyledStartButton>
 );
 
 export default StartButton;
