af9db3d786b614be3ff3f817bbcad53d27979751
Dustin Brett <dustinbrett@gmail.com>
2021-02-13 23:07:38 -0800

    Extracted ProcessesReducer

--- components/system/Processes/ProcessLoader.tsx
+++ components/system/Processes/ProcessLoader.tsx
@@ -1,13 +1,14 @@
 import RenderProcess from 'components/system/Processes/RenderProcess';
 import { ProcessConsumer } from 'contexts/process';
+import type { Process } from 'types/contexts/process';
+
+const ProcessesReducer = ([id, process]: [string, Process]) => (
+  <RenderProcess key={id} {...process} />
+);
 
 const ProcessLoader: React.FC = () => (
   <ProcessConsumer>
-    {({ processes }) =>
-      Object.entries(processes).map(([id, process]) => (
-        <RenderProcess key={id} {...process} />
-      ))
-    }
+    {({ processes }) => Object.entries(processes).map(ProcessesReducer)}
   </ProcessConsumer>
 );
 
