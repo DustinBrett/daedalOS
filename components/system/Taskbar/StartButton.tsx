a093d459428eb29c31b064e647d703721f15da8f
Dustin Brett <dustinbrett@gmail.com>
2021-02-20 21:36:37 -0800

    Set button type in styled component attributes

--- components/system/Taskbar/StartButton.tsx
+++ components/system/Taskbar/StartButton.tsx
@@ -1,9 +1,7 @@
 import StyledStartButton from 'styles/components/system/Taskbar/StyledStartButton';
 
 const StartButton = (): JSX.Element => (
-  <StyledStartButton type="button" title="Start">
-    X
-  </StyledStartButton>
+  <StyledStartButton title="Start">X</StyledStartButton>
 );
 
 export default StartButton;
--- styles/components/system/Taskbar/StyledStartButton.ts
+++ styles/components/system/Taskbar/StyledStartButton.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-const StyledStartButton = styled.button`
+const StyledStartButton = styled.button.attrs({
+  type: 'button'
+})`
   background-color: red;
   display: flex;
   height: 100%;
