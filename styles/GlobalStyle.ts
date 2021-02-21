13cee70c918c3905e92afe4dde5b7ad1b71aafcd
Dustin Brett <dustinbrett@gmail.com>
2021-02-20 22:19:07 -0800

    Added system font stack to body

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -11,6 +11,10 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
 
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
   button {
     background-color: unset;
   }
