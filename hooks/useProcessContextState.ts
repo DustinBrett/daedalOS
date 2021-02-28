dde1f05e2961171c9ac082b0227a93b680eeeaa8
Dustin Brett <dustinbrett@gmail.com>
2021-02-27 21:51:12 -0800

    Added type for process context state

--- hooks/useProcessContextState.ts
+++ hooks/useProcessContextState.ts
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import type { ProcessContextState } from 'types/contexts/process';
+import type { ProcessContextState, Processes } from 'types/contexts/process';
 import { Taskbar } from 'utils/processDirectory';
 
 const useProcessContextState = (): ProcessContextState => {
-  const [processes] = useState({ Taskbar });
+  const [processes] = useState<Processes>({ Taskbar });
 
   return { processes };
 };
