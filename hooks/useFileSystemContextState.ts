322672fc149c9630ff1778a68d862a14210dc152
Dustin Brett <dustinbrett@gmail.com>
2021-02-27 21:38:48 -0800

    Simplified file system context

--- hooks/useFileSystemContextState.ts
+++ hooks/useFileSystemContextState.ts
@@ -1,23 +1,17 @@
-import * as BrowserFS from 'browserfs';
+import { BFSRequire, configure } from 'browserfs';
 import type { FSModule } from 'browserfs/dist/node/core/FS';
 import { useEffect, useState } from 'react';
 import type { FileSystemContextState } from 'types/contexts/fileSystem';
+import FileSystemConfig from 'utils/FileSystemConfig';
 
 const useFileSystemContextState = (): FileSystemContextState => {
   const [fs, setFs] = useState<FSModule | null>(null);
 
   useEffect(() => {
-    BrowserFS.install(window);
-
-    BrowserFS.configure(
-      {
-        fs: 'IndexedDb'
-      },
-      () => {
-        setFs(BrowserFS.BFSRequire('fs'));
-      }
-    );
-  }, []);
+    if (!fs) {
+      configure(FileSystemConfig, () => setFs(BFSRequire('fs')));
+    }
+  }, [fs]);
 
   return { fs };
 };
---
+++ utils/FileSystemConfig.ts
@@ -0,0 +1,7 @@
+import type { FileSystemConfiguration } from 'browserfs';
+
+const FileSystemConfig: FileSystemConfiguration = {
+  fs: 'IndexedDb'
+};
+
+export default FileSystemConfig;
