3771503738eefea934ced4beb706c2b010a85704
Dustin Brett <dustinbrett@gmail.com>
2021-02-27 21:23:47 -0800

    Don't need renderProcesses

--- components/system/Processes/ProcessLoader.tsx
+++ components/system/Processes/ProcessLoader.tsx
@@ -1,15 +1,13 @@
 import RenderProcess from 'components/system/Processes/RenderProcess';
 import { ProcessConsumer } from 'contexts/process';
-import type { Processes } from 'types/contexts/process';
-
-const renderProcesses = (processes: Processes) =>
-  Object.entries(processes).map(([id, process]) => (
-    <RenderProcess key={id} {...process} />
-  ));
 
 const ProcessLoader = (): JSX.Element => (
   <ProcessConsumer>
-    {({ processes }) => renderProcesses(processes)}
+    {({ processes }) =>
+      Object.entries(processes).map(([id, process]) => (
+        <RenderProcess key={id} {...process} />
+      ))
+    }
   </ProcessConsumer>
 );
 
