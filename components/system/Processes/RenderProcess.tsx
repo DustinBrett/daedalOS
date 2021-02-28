040658f9ca40693ff83af9e601b833cf3169fb78
Dustin Brett <dustinbrett@gmail.com>
2021-02-27 21:26:34 -0800

    Added withWindow

--- components/system/Processes/RenderProcess.tsx
+++ components/system/Processes/RenderProcess.tsx
@@ -3,13 +3,13 @@ import type { Process } from 'types/contexts/process';
 
 const Window = dynamic(() => import('components/system/Window'));
 
-const RenderProcess = ({ Component, hasWindow }: Process): JSX.Element =>
-  hasWindow ? (
-    <Window>
-      <Component />
-    </Window>
-  ) : (
+const withWindow = (Component: React.ComponentType) => (
+  <Window>
     <Component />
-  );
+  </Window>
+);
+
+const RenderProcess = ({ Component, hasWindow }: Process): JSX.Element =>
+  hasWindow ? withWindow(Component) : <Component />;
 
 export default RenderProcess;
