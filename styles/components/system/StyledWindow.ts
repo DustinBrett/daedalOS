0b38a3fde77a852f3966eb6d325e816a97486909
Dustin Brett <dustinbrett@gmail.com>
2021-02-27 22:59:14 -0800

    Fixed window position

--- styles/components/system/StyledWindow.ts
+++ styles/components/system/StyledWindow.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const StyledWindow = styled.section`
   background-color: ${({ theme }) => theme.colors.window};
+  position: absolute;
 `;
 
 export default StyledWindow;
