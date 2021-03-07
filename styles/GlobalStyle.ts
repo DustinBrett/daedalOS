b871d47bca6968322bdcb6fb7a40a98126b77a53
Dustin Brett <dustinbrett@gmail.com>
2021-03-06 21:53:01 -0800

    Don't allow user select

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -13,6 +13,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    user-select: none;
   }
 
   button {
