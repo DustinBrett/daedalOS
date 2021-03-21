2e5b73a2fac24e1b3c1c4013771e5e34350dd783
Dustin Brett <dustinbrett@gmail.com>
2021-03-20 22:14:33 -0700

    Styled file manager grid for desktop

--- styles/components/system/Files/StyledFileManager.ts
+++ styles/components/system/Files/StyledFileManager.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
-const StyledFileManager = styled.ol``;
+const StyledFileManager = styled.ol`
+  column-gap: 1px;
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: repeat(auto-fill, 74px);
+  grid-template-rows: repeat(auto-fill, 70px);
+  height: ${({ theme }) => `calc(100% - ${theme.sizes.taskbar.height})`};
+  padding: 5px 0;
+  row-gap: 28px;
+`;
 
 export default StyledFileManager;
