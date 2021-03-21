9975d1222e09cb45452d884d68fd40da8ba53695
Dustin Brett <dustinbrett@gmail.com>
2021-03-20 21:58:38 -0700

    Update position when updating size

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -1,8 +1,7 @@
 import type { ProcessComponentProps } from 'components/system/Processes/RenderProcess';
 import Titlebar from 'components/system/Window/Titlebar';
 import { useProcesses } from 'contexts/process';
-import useDraggable from 'hooks/useDraggable';
-import useResizable from 'hooks/useResizeable';
+import useDraggableAndResizable from 'hooks/useDraggableAndResizable';
 import { Rnd } from 'react-rnd';
 import StyledWindow from 'styles/components/system/Window/StyledWindow';
 import rndDefaults from 'utils/rndDefaults';
@@ -13,8 +12,14 @@ const Window: React.FC<ProcessComponentProps> = ({ children, id }) => {
       [id]: { maximized, minimized }
     }
   } = useProcesses();
-  const { x, y, updatePosition } = useDraggable(maximized);
-  const { height, width, updateSize } = useResizable(maximized);
+  const {
+    height,
+    width,
+    updateSize,
+    x,
+    y,
+    updatePosition
+  } = useDraggableAndResizable(maximized);
 
   return (
     <Rnd
---
+++ hooks/useDraggableAndResizable.ts
@@ -0,0 +1,64 @@
+import { useCallback, useState } from 'react';
+import type { DraggableEventHandler } from 'react-draggable';
+import type { Position, RndResizeCallback } from 'react-rnd';
+import { useTheme } from 'styled-components';
+
+type Size = {
+  height: string;
+  width: string;
+};
+
+type Draggable = Position & {
+  updatePosition: DraggableEventHandler;
+};
+
+type Resizable = Size & {
+  updateSize: RndResizeCallback;
+};
+
+type DraggableAndResizable = Resizable & Draggable;
+
+const defaultWindowSize = {
+  height: '200px',
+  width: '250px'
+};
+
+const defaultWindowPosition = {
+  x: 0,
+  y: 0
+};
+
+const useDraggableAndResizable = (maximized = false): DraggableAndResizable => {
+  const { sizes } = useTheme();
+  const [{ height, width }, setSize] = useState<Size>(defaultWindowSize);
+  const [{ x, y }, setPosition] = useState<Position>(defaultWindowPosition);
+  const updatePosition = useCallback<DraggableEventHandler>(
+    (_event, { x: positionX, y: positionY }) =>
+      setPosition({ x: positionX, y: positionY }),
+    []
+  );
+  const updateSize = useCallback<RndResizeCallback>(
+    (
+      _event,
+      _direction,
+      { style: { height: elementHeight, width: elementWidth } },
+      _delta,
+      { x: positionX, y: positionY }
+    ) => {
+      setSize({ height: elementHeight, width: elementWidth });
+      setPosition({ x: positionX, y: positionY });
+    },
+    []
+  );
+
+  return {
+    x: maximized ? 0 : x,
+    y: maximized ? 0 : y,
+    updatePosition,
+    height: maximized ? `calc(100% - ${sizes.taskbar.height})` : height,
+    width: maximized ? '100%' : width,
+    updateSize
+  };
+};
+
+export default useDraggableAndResizable;
