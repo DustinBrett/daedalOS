118fe24474bff76f1add2499f521464784a8f2b3
Dustin Brett <dustinbrett@gmail.com>
2021-03-27 21:25:27 -0700

    Change Theme key to name

--- styles/themes.ts
+++ styles/themes.ts
@@ -2,7 +2,7 @@ import type { DefaultTheme } from 'styled-components';
 import defaultTheme from 'styles/defaultTheme';
 
 type Themes = {
-  [key: string]: DefaultTheme;
+  [name: string]: DefaultTheme;
 };
 
 const themes: Themes = { defaultTheme };
