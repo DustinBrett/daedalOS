3c885f1487f8557d315a93f2f9299eefab5f5bcc
Dustin Brett <dustinbrett@gmail.com>
2021-04-10 23:25:46 -0700

    Save session to file

--- contexts/session/useSessionContextState.ts
+++ contexts/session/useSessionContextState.ts
@@ -1,5 +1,6 @@
 import type { Size } from 'components/system/Window/RndWindow/useResizable';
-import { useState } from 'react';
+import { useFileSystem } from 'contexts/fileSystem';
+import { useEffect, useState } from 'react';
 import type { Position } from 'react-rnd';
 
 type WindowState = {
@@ -18,9 +19,40 @@ export type SessionContextState = {
   windowStates: WindowStates;
 };
 
+const SESSION_FILE = '/session.json';
+
 const useSessionContextState = (): SessionContextState => {
+  const { fs } = useFileSystem();
+  const [sessionLoaded, setSessionLoaded] = useState(false);
   const [themeName, setThemeName] = useState('');
   const [windowStates, setWindowStates] = useState<WindowStates>({});
+
+  useEffect(() => {
+    if (sessionLoaded) {
+      fs?.writeFile(
+        SESSION_FILE,
+        JSON.stringify({
+          themeName,
+          windowStates
+        })
+      );
+    }
+  }, [fs, sessionLoaded, themeName, windowStates]);
+
+  useEffect(
+    () =>
+      fs?.readFile(SESSION_FILE, (_error, contents) => {
+        if (contents) {
+          const session = JSON.parse(contents.toString());
+
+          setThemeName(session.themeName);
+          setWindowStates(session.windowStates);
+        }
+
+        setSessionLoaded(true);
+      }),
+    [fs]
+  );
 
   return { setThemeName, setWindowStates, themeName, windowStates };
 };
