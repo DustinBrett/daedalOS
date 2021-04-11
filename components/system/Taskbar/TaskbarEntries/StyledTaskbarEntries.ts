0c54704e836c2c49b8395ad3f179013da5fdc3db
Dustin Brett <dustinbrett@gmail.com>
2021-04-10 22:43:48 -0700

    Tweaks to taskbar entries

--- components/system/Taskbar/TaskbarEntries/StyledTaskbarEntries.ts
+++ components/system/Taskbar/TaskbarEntries/StyledTaskbarEntries.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 const StyledTaskbarEntries = styled.ol`
+  column-gap: 1px;
   display: flex;
   height: 100%;
   left: ${({ theme }) => theme.sizes.startButton.width};
+  margin: 0 3px;
   position: absolute;
   right: ${({ theme }) => theme.sizes.clock.width};
 `;
