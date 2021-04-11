385e9ed82e2d7bcee807b8a7c41a30e9f0eb0505
Dustin Brett <dustinbrett@gmail.com>
2021-04-10 22:07:55 -0700

    Compare extensions as lowercase

--- components/system/Files/useFileInfo.ts
+++ components/system/Files/useFileInfo.ts
@@ -25,7 +25,7 @@ const useFileInfo = (path: string): FileInfo => {
 
   useEffect(() => {
     if (fs) {
-      const extension = extname(path);
+      const extension = extname(path).toLowerCase();
       const getInfoByFileExtension = () =>
         setInfo({
           icon: getIconByFileExtension(extension),
--- utils/functions.ts
+++ utils/functions.ts
@@ -1,5 +1,8 @@
 import { extname } from 'path';
 
+export const bufferToUrl = (buffer: Buffer): string =>
+  URL.createObjectURL(new Blob([new Uint8Array(buffer)]));
+
 export const loadScript = (src: string): Promise<Event> =>
   new Promise((resolve, reject) => {
     const loadedScripts = [...document.scripts];
@@ -39,7 +42,7 @@ export const loadStyle = (href: string): Promise<Event> =>
 export const loadFiles = async (files: string[]): Promise<Event[]> =>
   Promise.all(
     files.reduce((filesToLoad: Promise<Event>[], file) => {
-      const ext = extname(file);
+      const ext = extname(file).toLowerCase();
 
       if (ext === '.css') filesToLoad.push(loadStyle(file));
       else if (ext === '.js') filesToLoad.push(loadScript(file));
@@ -47,6 +50,3 @@ export const loadFiles = async (files: string[]): Promise<Event[]> =>
       return filesToLoad;
     }, [])
   );
-
-export const bufferToUrl = (buffer: Buffer): string =>
-  URL.createObjectURL(new Blob([new Uint8Array(buffer)]));
