3eae7f1a4d74013bc5128cb8f4d2c83c84117452
Dustin Brett <dustinbrett@gmail.com>
2021-04-17 22:52:15 -0700

    Add Html lang back into `_document`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,25 @@
 import type { DocumentContext, DocumentInitialProps } from 'next/document';
-import Document from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import withStyledComponents from 'styles/withStyledComponents';
+import { DEFAULT_LOCALE } from 'utils/constants';
 
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
     return withStyledComponents(ctx);
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang={DEFAULT_LOCALE}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
