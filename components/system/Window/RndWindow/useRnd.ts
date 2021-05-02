fb479c76bbb0d85748ad812b99afe9b9ca045cf2
Dustin Brett <dustinbrett@gmail.com>
2021-05-01 22:44:30 -0700

    Centered initial window position

--- components/system/Window/RndWindow/useRnd.ts
+++ components/system/Window/RndWindow/useRnd.ts
@@ -1,11 +1,20 @@
 import rndDefaults from 'components/system/Window/RndWindow/rndDefaults';
 import useDraggable from 'components/system/Window/RndWindow/useDraggable';
+import type { Size } from 'components/system/Window/RndWindow/useResizable';
 import useResizable from 'components/system/Window/RndWindow/useResizable';
 import { useProcesses } from 'contexts/process';
 import { useSession } from 'contexts/session';
 import { useCallback } from 'react';
 import type { DraggableEventHandler } from 'react-draggable';
-import type { Props, RndResizeCallback } from 'react-rnd';
+import type { Position, Props, RndResizeCallback } from 'react-rnd';
+import { useTheme } from 'styled-components';
+import { pxToNum } from 'utils/functions';
+
+const centerPosition = (size: Size, taskbarHeight: string): Position => ({
+  x: window.innerWidth / 2 - pxToNum(size.width) / 2,
+  y:
+    (window.innerHeight - pxToNum(taskbarHeight)) / 2 - pxToNum(size.height) / 2
+});
 
 const useRnd = (id: string, maximized = false): Props => {
   const {
@@ -15,8 +24,16 @@ const useRnd = (id: string, maximized = false): Props => {
   } = useProcesses();
   const { windowStates: { [id]: windowState } = {} } = useSession();
   const { position: statePosition, size: stateSize } = windowState || {};
-  const [position, setPosition] = useDraggable(maximized, statePosition);
+  const {
+    sizes: {
+      taskbar: { height: taskbarHeight }
+    }
+  } = useTheme();
   const [size, setSize] = useResizable(maximized, autoSizing, stateSize);
+  const [position, setPosition] = useDraggable(
+    maximized,
+    statePosition || centerPosition(size, taskbarHeight)
+  );
   const onDragStop = useCallback<DraggableEventHandler>(
     (_event, { x: positionX, y: positionY }) =>
       setPosition({ x: positionX, y: positionY }),
