fe1ec8ad33ce956a09b6a479946c1386d9bb9439
Dustin Brett <dustinbrett@gmail.com>
2021-05-08 23:14:55 -0700

    `enableResizing` can check `autoSizing` once

--- components/system/Window/RndWindow/useRnd.ts
+++ components/system/Window/RndWindow/useRnd.ts
@@ -58,8 +58,7 @@ const useRnd = (id: string, maximized = false): Props => {
 
   return {
     disableDragging: maximized,
-    enableResizing:
-      !maximized && (!autoSizing || (autoSizing && lockAspectRatio)),
+    enableResizing: !maximized && (!autoSizing || lockAspectRatio),
     lockAspectRatio,
     onDragStop,
     onResizeStop,
