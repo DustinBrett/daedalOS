6e923a255e1ca95fdae6628cd48295ee2a832cae
Dustin Brett <dustinbrett@gmail.com>
2021-05-15 23:28:11 -0700

    Webamp can load MP3 + refactored

--- components/apps/Webamp/index.tsx
+++ components/apps/Webamp/index.tsx
@@ -1,70 +1,29 @@
-import {
-  closeEqualizer,
-  getWebampElement,
-  updateWebampPosition
-} from 'components/apps/Webamp/functions';
-import type { WebampCI } from 'components/apps/Webamp/types';
+import { getWebampElement } from 'components/apps/Webamp/functions';
+import useWebamp from 'components/apps/Webamp/useWebamp';
 import type { ComponentProcessProps } from 'components/system/Apps/RenderComponent';
+import { useFileSystem } from 'contexts/fileSystem';
 import { useProcesses } from 'contexts/process';
-import { useSession } from 'contexts/session';
 import { useEffect, useRef } from 'react';
-import { useTheme } from 'styled-components';
 import { loadFiles } from 'utils/functions';
 
 const Webamp = ({ id }: ComponentProcessProps): JSX.Element => {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const {
-    close,
-    minimize,
-    processes: { [id]: { minimized = false } = {} } = {}
-  } = useProcesses();
-  const {
-    setWindowStates,
-    windowStates: { [id]: { position = undefined } = {} } = {}
-  } = useSession();
-  const {
-    sizes: {
-      taskbar: { height: taskbarHeight }
-    }
-  } = useTheme();
+  const { fs } = useFileSystem();
+  const { processes: { [id]: { minimized = false, url = '' } = {} } = {} } =
+    useProcesses();
+  const { loadWebamp } = useWebamp(id);
 
   useEffect(() => {
-    loadFiles(['/libs/webamp/webamp.bundle.min.js']).then(() => {
-      if (containerRef?.current) {
-        const webamp: WebampCI = new window.Webamp({ zIndex: 1 });
+    if (fs) {
+      fs?.readFile(url, (_error, contents) => {
+        loadFiles(['/libs/webamp/webamp.bundle.min.js']).then(() =>
+          loadWebamp(containerRef?.current, contents)
+        );
+      });
+    }
+  }, [containerRef, fs, loadWebamp, url]);
 
-        webamp.onClose(() => {
-          const [main] = getWebampElement().getElementsByClassName('window');
-          const { x, y } = main.getBoundingClientRect();
-
-          close(id);
-          setWindowStates((currentWindowStates) => ({
-            ...currentWindowStates,
-            [id]: {
-              position: { x, y }
-            }
-          }));
-        });
-        webamp.onMinimize(() => minimize(id));
-        webamp
-          .renderWhenReady(containerRef?.current as HTMLDivElement)
-          .then(() => {
-            closeEqualizer(webamp);
-            updateWebampPosition(webamp, taskbarHeight, position);
-            containerRef?.current?.appendChild(getWebampElement());
-          });
-      }
-    });
-  }, [
-    close,
-    containerRef,
-    id,
-    minimize,
-    position,
-    setWindowStates,
-    taskbarHeight
-  ]);
-
+  // TODO: Replace with Framer animation
   useEffect(() => {
     const webamp = getWebampElement();
 
---
+++ components/apps/Webamp/useWebamp.ts
@@ -0,0 +1,81 @@
+import {
+  closeEqualizer,
+  getWebampElement,
+  updateWebampPosition
+} from 'components/apps/Webamp/functions';
+import type { WebampCI } from 'components/apps/Webamp/types';
+import { useProcesses } from 'contexts/process';
+import { useSession } from 'contexts/session';
+import { useCallback } from 'react';
+import { useTheme } from 'styled-components';
+import { bufferToUrl, cleanUpBufferUrl } from 'utils/functions';
+
+type Webamp = {
+  loadWebamp: (containerElement: HTMLDivElement | null, file?: Buffer) => void;
+};
+
+const useWebamp = (id: string): Webamp => {
+  const { close, minimize } = useProcesses();
+  const {
+    setWindowStates,
+    windowStates: { [id]: { position = undefined } = {} } = {}
+  } = useSession();
+  const {
+    sizes: {
+      taskbar: { height: taskbarHeight }
+    }
+  } = useTheme();
+  const loadWebamp = useCallback(
+    (containerElement: HTMLDivElement | null, file?: Buffer): void => {
+      if (containerElement) {
+        const options: any = { zIndex: 1 }; // TODO: Base zIndex on foregroundId === id
+
+        if (file) {
+          options.initialTracks = [
+            {
+              metaData: {
+                artist: '',
+                title: ''
+              },
+              url: bufferToUrl(file)
+            }
+          ];
+        }
+
+        const webamp: WebampCI = new window.Webamp(options);
+
+        webamp.onClose(() => {
+          const [main] = getWebampElement().getElementsByClassName('window');
+          const { x, y } = main.getBoundingClientRect();
+
+          close(id);
+          setWindowStates((currentWindowStates) => ({
+            ...currentWindowStates,
+            [id]: {
+              position: { x, y }
+            }
+          }));
+
+          if (options.initialTracks) {
+            const [{ url: objectUrl }] = options.initialTracks;
+
+            cleanUpBufferUrl(objectUrl);
+          }
+        });
+        webamp.onMinimize(() => minimize(id));
+        webamp.renderWhenReady(containerElement).then(() => {
+          closeEqualizer(webamp);
+          updateWebampPosition(webamp, taskbarHeight, position);
+          containerElement.appendChild(getWebampElement());
+        });
+      }
+    },
+    [close, id, minimize, position, setWindowStates, taskbarHeight]
+  );
+
+  return {
+    loadWebamp
+  };
+};
+
+export default useWebamp;
