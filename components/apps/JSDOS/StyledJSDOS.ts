80acb766d5636d99b9d34c0218ae6ab647ff3725
Dustin Brett <dustinbrett@gmail.com>
2021-05-15 21:30:22 -0700

    Fixed JSDOS maximize aspect ratio

--- components/apps/JSDOS/StyledJSDOS.ts
+++ components/apps/JSDOS/StyledJSDOS.ts
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 const StyledJSDOS = styled.div`
+  align-items: center;
   background-color: #000;
+  display: flex;
   height: ${({ theme }) =>
     `calc(100% - ${theme.sizes.titleBar.height}) !important`};
   width: 100%;
 
   canvas {
-    height: 100% !important;
     image-rendering: pixelated;
     image-rendering: crisp-edges;
     top: 0 !important;
