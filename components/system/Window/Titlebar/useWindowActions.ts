7c6b62d5005a4e953449435800fbce709d278615
Dustin Brett <dustinbrett@gmail.com>
2021-06-12 22:59:39 -0700

    Remove useCallback from useWindowActions

--- components/system/Window/Titlebar/useWindowActions.ts
+++ components/system/Window/Titlebar/useWindowActions.ts
@@ -2,7 +2,6 @@ import { closeWithTransition } from 'components/system/Window/functions';
 import useNextFocusable from 'components/system/Window/useNextFocusable';
 import { useProcesses } from 'contexts/process';
 import { useSession } from 'contexts/session';
-import { useCallback } from 'react';
 
 type WindowActions = {
   onClose: () => void;
@@ -14,16 +13,16 @@ const useWindowActions = (id: string): WindowActions => {
   const nextFocusableId = useNextFocusable(id);
   const { setForegroundId, removeFromStack } = useSession();
   const { close, maximize, minimize } = useProcesses();
-  const onMinimize = useCallback(() => {
+  const onMinimize = () => {
     minimize(id);
     setForegroundId(nextFocusableId);
-  }, [id, minimize, nextFocusableId, setForegroundId]);
-  const onMaximize = useCallback(() => maximize(id), [id, maximize]);
-  const onClose = useCallback(() => {
+  };
+  const onMaximize = () => maximize(id);
+  const onClose = () => {
     removeFromStack(id);
     closeWithTransition(close, id);
     setForegroundId(nextFocusableId);
-  }, [removeFromStack, id, close, setForegroundId, nextFocusableId]);
+  };
 
   return { onClose, onMaximize, onMinimize };
 };
