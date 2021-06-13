c91dbcdf15699d920cd06e2e07840d7054504ff8
Dustin Brett <dustinbrett@gmail.com>
2021-06-12 22:56:27 -0700

    Removed useCallback from useRnd

--- components/system/Window/RndWindow/useRnd.ts
+++ components/system/Window/RndWindow/useRnd.ts
@@ -4,7 +4,6 @@ import useDraggable from 'components/system/Window/RndWindow/useDraggable';
 import useResizable from 'components/system/Window/RndWindow/useResizable';
 import { useProcesses } from 'contexts/process';
 import { useSession } from 'contexts/session';
-import { useCallback } from 'react';
 import type { DraggableEventHandler } from 'react-draggable';
 import type { Props, RndResizeCallback } from 'react-rnd';
 import { useTheme } from 'styled-components';
@@ -24,24 +23,20 @@ const useRnd = (id: string, maximized = false): Props => {
   const [position, setPosition] = useDraggable(
     statePosition || centerPosition(size, taskbarHeight)
   );
-  const onDragStop = useCallback<DraggableEventHandler>(
-    (_event, { x: positionX, y: positionY }) =>
-      setPosition({ x: positionX, y: positionY }),
-    [setPosition]
-  );
-  const onResizeStop = useCallback<RndResizeCallback>(
-    (
-      _event,
-      _direction,
-      { style: { height: elementHeight, width: elementWidth } },
-      _delta,
-      { x: positionX, y: positionY }
-    ) => {
-      setSize({ height: elementHeight, width: elementWidth });
-      setPosition({ x: positionX, y: positionY });
-    },
-    [setPosition, setSize]
-  );
+  const onDragStop: DraggableEventHandler = (
+    _event,
+    { x: positionX, y: positionY }
+  ) => setPosition({ x: positionX, y: positionY });
+  const onResizeStop: RndResizeCallback = (
+    _event,
+    _direction,
+    { style: { height: elementHeight, width: elementWidth } },
+    _delta,
+    { x: positionX, y: positionY }
+  ) => {
+    setSize({ height: elementHeight, width: elementWidth });
+    setPosition({ x: positionX, y: positionY });
+  };
 
   return {
     disableDragging: maximized,
