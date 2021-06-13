eba1b23afef7451320497605cd42ee0fa4c300d5
Dustin Brett <dustinbrett@gmail.com>
2021-06-12 23:29:40 -0700

    Clean icon buffer on destroy

---
+++ styles/common/Icon.tsx
@@ -0,0 +1,42 @@
+import { basename, dirname } from 'path';
+import { memo, useEffect } from 'react';
+import styled from 'styled-components';
+import { cleanUpBufferUrl } from 'utils/functions';
+
+export type IconProps = {
+  displaySize?: number;
+  imgSize: number;
+};
+
+const onLoad: React.ReactEventHandler = ({ target }) =>
+  (target as HTMLImageElement).style.setProperty('visibility', 'visible');
+
+const StyledIcon = styled.img.attrs<IconProps>(
+  ({ imgSize, displaySize, src = '' }) => ({
+    draggable: false,
+    height: `${displaySize || imgSize}px`,
+    onLoad,
+    src:
+      !src || src.startsWith('blob:')
+        ? src
+        : `${dirname(src)}/${imgSize}x${imgSize}/${basename(src)}`,
+    width: `${displaySize || imgSize}px`
+  })
+)<IconProps>`
+  visibility: hidden;
+`;
+
+const Icon = (
+  props: React.ImgHTMLAttributes<HTMLImageElement> & IconProps
+): JSX.Element => {
+  useEffect(
+    () => () => {
+      if (props?.src?.startsWith('blob:')) cleanUpBufferUrl(props?.src);
+    },
+    [props]
+  );
+
+  return <StyledIcon {...props} />;
+};
+
+export default memo(Icon);
