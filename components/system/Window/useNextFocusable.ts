d52f90c898facf76d94ae93c384f7fd1e5389cd5
Dustin Brett <dustinbrett@gmail.com>
2021-06-12 23:10:55 -0700

    Removed useMemo from useNextFocusable

--- components/system/Window/useNextFocusable.ts
+++ components/system/Window/useNextFocusable.ts
@@ -1,17 +1,12 @@
 import { useProcesses } from 'contexts/process';
 import { useSession } from 'contexts/session';
-import { useMemo } from 'react';
 
 const useNextFocusable = (id: string): string => {
   const { stackOrder } = useSession();
   const { processes } = useProcesses();
 
-  const nextFocusableId = useMemo(
-    () =>
-      stackOrder.find(
-        (stackId) => stackId !== id && !processes?.[stackId]?.minimized
-      ),
-    [id, processes, stackOrder]
+  const nextFocusableId = stackOrder.find(
+    (stackId) => stackId !== id && !processes?.[stackId]?.minimized
   );
 
   return nextFocusableId || '';
