b346cc172130bba1baa8b5b5f14744e90897e836
Dustin Brett <dustinbrett@gmail.com>
2021-06-12 22:18:59 -0700

    Added margin to left of file manager list view

--- components/system/Files/Views/List/StyledFileManager.ts
+++ components/system/Files/Views/List/StyledFileManager.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const StyledFileManager = styled.ol`
-  padding-top: 7px;
+  margin-left: 7px;
+  margin-top: 7px;
 `;
 
 export default StyledFileManager;
