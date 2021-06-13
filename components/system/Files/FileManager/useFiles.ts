2ca306815a44705d484c72b9bb16804df1a9b019
Dustin Brett <dustinbrett@gmail.com>
2021-06-12 22:13:07 -0700

    Remove useCallback fro useFiles

--- components/system/Files/FileManager/useFiles.ts
+++ components/system/Files/FileManager/useFiles.ts
@@ -1,6 +1,6 @@
 import { useFileSystem } from 'contexts/fileSystem';
 import { basename, extname } from 'path';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type Files = {
   deleteFile: (path: string) => void;
@@ -12,42 +12,33 @@ type Files = {
 const useFiles = (directory: string): Files => {
   const [files, setFiles] = useState<string[]>([]);
   const { fs } = useFileSystem();
-  const updateFiles = useCallback(
-    (appendFile = '') =>
-      fs?.readdir(directory, (_error, contents = []) =>
-        setFiles((currentFiles) =>
-          appendFile ? [...currentFiles, basename(appendFile)] : contents
-        )
-      ),
-    [directory, fs]
-  );
-  const deleteFile = useCallback(
-    (path: string) =>
-      fs?.unlink(path, () =>
-        setFiles((currentFiles) =>
-          currentFiles.filter((file) => file !== basename(path))
-        )
-      ),
-    [fs]
-  );
-  const renameFile = useCallback(
-    (path: string, name?: string) => {
-      if (name) {
-        const newPath = `${directory}/${name}${extname(path)}`;
+  const updateFiles = (appendFile = '') =>
+    fs?.readdir(directory, (_error, contents = []) =>
+      setFiles((currentFiles) =>
+        appendFile ? [...currentFiles, basename(appendFile)] : contents
+      )
+    );
+  const deleteFile = (path: string) =>
+    fs?.unlink(path, () =>
+      setFiles((currentFiles) =>
+        currentFiles.filter((file) => file !== basename(path))
+      )
+    );
+  const renameFile = (path: string, name?: string) => {
+    if (name) {
+      const newPath = `${directory}/${name}${extname(path)}`;
 
-        fs?.rename(path, newPath, () =>
-          setFiles((currentFiles) =>
-            currentFiles.map((file) =>
-              file.replace(basename(path), basename(newPath))
-            )
+      fs?.rename(path, newPath, () =>
+        setFiles((currentFiles) =>
+          currentFiles.map((file) =>
+            file.replace(basename(path), basename(newPath))
           )
-        );
-      }
-    },
-    [directory, fs]
-  );
+        )
+      );
+    }
+  };
 
-  useEffect(updateFiles, [updateFiles]);
+  useEffect(updateFiles, [directory, fs]);
 
   return {
     deleteFile,
