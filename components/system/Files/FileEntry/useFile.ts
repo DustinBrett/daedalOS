d83968b2a23085dc085a8b409e1417ccabf513cd
Dustin Brett <dustinbrett@gmail.com>
2021-06-12 21:58:12 -0700

    Removed useCallback from Open file

--- components/system/Files/FileEntry/useFile.ts
+++ components/system/Files/FileEntry/useFile.ts
@@ -1,14 +1,13 @@
 import { useProcesses } from 'contexts/process';
 import { createPid } from 'contexts/process/functions';
 import { useSession } from 'contexts/session';
-import { useCallback } from 'react';
 
 type UseFile = () => void;
 
 const useFile = (url: string, pid: string): UseFile => {
   const { setForegroundId } = useSession();
   const { minimize, open, processes } = useProcesses();
-  const openFile = useCallback(() => {
+  const openFile = () => {
     const id = createPid(pid, url);
 
     if (processes[id]) {
@@ -17,7 +16,7 @@ const useFile = (url: string, pid: string): UseFile => {
     } else {
       open(pid, url);
     }
-  }, [minimize, open, pid, processes, setForegroundId, url]);
+  };
 
   return openFile;
 };
