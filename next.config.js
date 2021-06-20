064ca1084dc96bad7aaccc11badca03d13cb8e14
Dustin Brett <dustinbrett@gmail.com>
2021-06-19 22:39:49 -0700

    Next js config file type checking

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+// @ts-check
+
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ * */
+const nextConfig = {
+  experimental: {},
+  future: {},
   reactStrictMode: true
 };
+
+module.exports = nextConfig;
