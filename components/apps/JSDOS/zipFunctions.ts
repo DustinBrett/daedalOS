cf88bf157f54f522c373ff4ebeba50e959f1c697
Dustin Brett <dustinbrett@gmail.com>
2021-06-26 22:41:01 -0700

    Refactor addFileToZippable

--- components/apps/JSDOS/zipFunctions.ts
+++ components/apps/JSDOS/zipFunctions.ts
@@ -2,22 +2,17 @@ import type { FSModule } from 'browserfs/dist/node/core/FS';
 import type { AsyncZippable } from 'fflate';
 import { unzip, zip } from 'fflate';
 
-const addFileToZippable = (path: string, file: Buffer): AsyncZippable => {
-  const zippableData: AsyncZippable = {};
-
-  path.split('/').reduce((walkedPath, pathPart, index, { length }) => {
+const addFileToZippable = (path: string, file: Buffer): AsyncZippable =>
+  path.split('/').reduce((zippableData, pathPart, index, { length }) => {
     const endOfPath = index === length - 1;
+    const walkedPath = Object.keys(zippableData)[index - 1] || '';
     const currentPath = `${walkedPath}${pathPart}${endOfPath ? '' : '/'}`;
 
-    zippableData[currentPath] = endOfPath
-      ? [file, { level: 0 }]
-      : new Uint8Array();
-
-    return currentPath;
-  }, '');
-
-  return zippableData;
-};
+    return {
+      ...zippableData,
+      [currentPath]: endOfPath ? [file, { level: 0 }] : new Uint8Array()
+    };
+  }, {} as AsyncZippable);
 
 export const addFileToZip = (
   buffer: Buffer,
