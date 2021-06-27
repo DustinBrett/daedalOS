0126d837531ea924962779a3ab2a94a317f9d5d3
Dustin Brett <dustinbrett@gmail.com>
2021-06-26 22:41:30 -0700

    Add missing updateFiles dep

--- components/system/Files/FileManager/useFiles.ts
+++ components/system/Files/FileManager/useFiles.ts
@@ -44,7 +44,7 @@ const useFiles = (directory: string): Files => {
     }
   };
 
-  useEffect(updateFiles, [directory, fs]);
+  useEffect(updateFiles, [directory, fs, updateFiles]);
 
   return {
     deleteFile,
