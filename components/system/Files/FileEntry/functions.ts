e29a28800aa19e58be05dfa6404fd167e15df100
Dustin Brett <dustinbrett@gmail.com>
2021-07-03 21:46:40 -0700

    Extracted functions from useFileInfo

--- components/system/Files/FileEntry/functions.ts
+++ components/system/Files/FileEntry/functions.ts
@@ -1,10 +1,78 @@
+import type { FSModule } from "browserfs/dist/node/core/FS";
 import extensions from "components/system/Files/FileEntry/extensions";
+import type { FileInfo } from "components/system/Files/FileEntry/useFileInfo";
+import ini from "ini";
+import { parseBuffer } from "music-metadata-browser";
+import { IMAGE_FILE_EXTENSIONS, SHORTCUT_EXTENSION } from "utils/constants";
+import { bufferToUrl } from "utils/functions";
 
-export const getIconByFileExtension = (extension: string): string =>
+const getIconByFileExtension = (extension: string): string =>
   `/icons/${extensions[extension]?.icon || "unknown"}.png`;
 
-export const getProcessByFileExtension = (extension: string): string => {
+const getProcessByFileExtension = (extension: string): string => {
   const [defaultProcess = ""] = extensions[extension]?.process || [];
 
   return defaultProcess;
+};
+
+export const getInfoWithoutExtension = (
+  fs: FSModule,
+  path: string,
+  callback: React.Dispatch<React.SetStateAction<FileInfo>>
+): void =>
+  fs.stat(path, (_error, stats) => {
+    const isDirectory = stats ? stats.isDirectory() : false;
+
+    callback({
+      icon: `/icons/${isDirectory ? "folder.png" : "unknown.png"}`,
+      pid: isDirectory ? "FileExplorer" : "",
+      url: path,
+    });
+  });
+
+export const getInfoWithExtension = (
+  fs: FSModule,
+  path: string,
+  extension: string,
+  callback: React.Dispatch<React.SetStateAction<FileInfo>>
+): void => {
+  const getInfoByFileExtension = (icon?: string) =>
+    callback({
+      icon: icon || getIconByFileExtension(extension),
+      pid: getProcessByFileExtension(extension),
+      url: path,
+    });
+
+  if (extension === SHORTCUT_EXTENSION) {
+    fs.readFile(path, (error, contents = Buffer.from("")) => {
+      if (error) {
+        getInfoByFileExtension();
+      } else {
+        const {
+          InternetShortcut: {
+            BaseURL: pid = "",
+            IconFile: icon = "",
+            URL: url = "",
+          },
+        } = ini.parse(contents.toString());
+
+        callback({ icon, pid, url });
+      }
+    });
+  } else if (IMAGE_FILE_EXTENSIONS.has(extension)) {
+    fs.readFile(path, (error, contents = Buffer.from("")) =>
+      getInfoByFileExtension(error ? "/icons/photo.png" : bufferToUrl(contents))
+    );
+  } else if (extension === ".mp3") {
+    fs.readFile(path, (error, contents = Buffer.from("")) =>
+      parseBuffer(contents).then(
+        ({ common: { picture: [picture] = [] } = {} }) =>
+          getInfoByFileExtension(
+            !error && picture ? bufferToUrl(picture.data) : "/icons/music.png"
+          )
+      )
+    );
+  } else {
+    getInfoByFileExtension();
+  }
 };
--- components/system/Files/FileEntry/useFileInfo.ts
+++ components/system/Files/FileEntry/useFileInfo.ts
@@ -1,16 +1,12 @@
 import {
-  getIconByFileExtension,
-  getProcessByFileExtension,
+  getInfoWithExtension,
+  getInfoWithoutExtension,
 } from "components/system/Files/FileEntry/functions";
 import { useFileSystem } from "contexts/fileSystem";
-import ini from "ini";
-import { parseBuffer } from "music-metadata-browser";
 import { extname } from "path";
 import { useEffect, useState } from "react";
-import { IMAGE_FILE_EXTENSIONS, SHORTCUT_EXTENSION } from "utils/constants";
-import { bufferToUrl } from "utils/functions";
 
-type FileInfo = {
+export type FileInfo = {
   icon: string;
   pid: string;
   url: string;
@@ -29,59 +25,9 @@ const useFileInfo = (path: string): FileInfo => {
       const extension = extname(path).toLowerCase();
 
       if (!extension) {
-        fs.stat(path, (_error, stats) => {
-          const isDirectory = stats ? stats.isDirectory() : false;
-
-          setInfo({
-            icon: `/icons/${isDirectory ? "folder.png" : "unknown.png"}`,
-            pid: isDirectory ? "FileExplorer" : "",
-            url: path,
-          });
-        });
+        getInfoWithoutExtension(fs, path, setInfo);
       } else {
-        const getInfoByFileExtension = (icon?: string) =>
-          setInfo({
-            icon: icon || getIconByFileExtension(extension),
-            pid: getProcessByFileExtension(extension),
-            url: path,
-          });
-
-        if (extension === SHORTCUT_EXTENSION) {
-          fs.readFile(path, (error, contents = Buffer.from("")) => {
-            if (error) {
-              getInfoByFileExtension();
-            } else {
-              const {
-                InternetShortcut: {
-                  BaseURL: pid = "",
-                  IconFile: icon = "",
-                  URL: url = "",
-                },
-              } = ini.parse(contents.toString());
-
-              setInfo({ icon, pid, url });
-            }
-          });
-        } else if (IMAGE_FILE_EXTENSIONS.has(extension)) {
-          fs.readFile(path, (error, contents = Buffer.from("")) =>
-            getInfoByFileExtension(
-              error ? "/icons/photo.png" : bufferToUrl(contents)
-            )
-          );
-        } else if (extension === ".mp3") {
-          fs.readFile(path, (error, contents = Buffer.from("")) =>
-            parseBuffer(contents).then(
-              ({ common: { picture: [picture] = [] } = {} }) =>
-                getInfoByFileExtension(
-                  !error && picture
-                    ? bufferToUrl(picture.data)
-                    : "/icons/music.png"
-                )
-            )
-          );
-        } else {
-          getInfoByFileExtension();
-        }
+        getInfoWithExtension(fs, path, extension, setInfo);
       }
     }
   }, [fs, path]);
