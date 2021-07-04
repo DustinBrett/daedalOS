a8f10c535aaa82e0594fbab665904315273920d2
Dustin Brett <dustinbrett@gmail.com>
2021-07-03 23:28:37 -0700

    Lazy load StartMenu component

--- components/system/Taskbar/index.tsx
+++ components/system/Taskbar/index.tsx
@@ -1,9 +1,11 @@
-import StartMenu from "components/system/StartMenu";
 import Clock from "components/system/Taskbar/Clock";
 import StartButton from "components/system/Taskbar/StartButton";
 import StyledTaskbar from "components/system/Taskbar/StyledTaskbar";
 import TaskbarEntries from "components/system/Taskbar/TaskbarEntries";
 import { useSession } from "contexts/session";
+import dynamic from "next/dynamic";
+
+const StartMenu = dynamic(() => import("components/system/StartMenu"));
 
 const Taskbar = (): JSX.Element => {
   const { startMenuVisible } = useSession();
