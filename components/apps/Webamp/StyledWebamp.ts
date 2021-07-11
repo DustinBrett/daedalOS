8e9b738a28f435eb80fb998b44b055533fcc767f
Dustin Brett <dustinbrett@gmail.com>
2021-07-10 23:21:54 -0700

    Use inset when possible

--- components/apps/Webamp/StyledWebamp.ts
+++ components/apps/Webamp/StyledWebamp.ts
@@ -2,12 +2,9 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 
 const StyledWebamp = styled(motion.div)`
-  bottom: 0;
-  left: 0;
+  inset: 0;
   pointer-events: none;
   position: absolute;
-  right: 0;
-  top: 0;
 
   div:first-child {
     pointer-events: auto;
--- components/system/Desktop/StyledDesktop.ts
+++ components/system/Desktop/StyledDesktop.ts
@@ -2,12 +2,9 @@ import styled from "styled-components";
 
 const StyledDesktop = styled.main`
   background-color: ${({ theme }) => theme.colors.background};
-  bottom: 0;
   height: 100vh;
-  left: 0;
+  inset: 0;
   position: fixed;
-  right: 0;
-  top: 0;
   width: 100vw;
 `;
 
