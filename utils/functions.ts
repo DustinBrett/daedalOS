dca6bde6691d669026fbb188df641b02d3e88344
Dustin Brett <dustinbrett@gmail.com>
2021-07-10 22:57:49 -0700

    Never load styles dynamically

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,4 +1,3 @@
-import { extname } from "path";
 import { stripUnit } from "polished";
 import {
   DOUBLE_CLICK_TIMEOUT_IN_MILLISECONDS,
@@ -31,35 +30,8 @@ export const loadScript = (src: string): Promise<Event> =>
     }
   });
 
-export const loadStyle = (href: string): Promise<Event> =>
-  new Promise((resolve, reject) => {
-    const loadedLinks = [...document.querySelectorAll("link")];
-
-    if (loadedLinks.some((link) => link.href.endsWith(href))) {
-      resolve(new Event("Already loaded."));
-    } else {
-      const link = document.createElement("link");
-
-      link.rel = "stylesheet";
-      link.href = href;
-      link.addEventListener("error", reject, ONE_TIME_PASSIVE_EVENT);
-      link.addEventListener("load", resolve, ONE_TIME_PASSIVE_EVENT);
-
-      document.head.appendChild(link);
-    }
-  });
-
 export const loadFiles = async (files: string[]): Promise<Event[]> =>
-  Promise.all(
-    files.reduce((filesToLoad: Promise<Event>[], file) => {
-      const ext = extname(file).toLowerCase();
-
-      if (ext === ".css") filesToLoad.push(loadStyle(file));
-      else if (ext === ".js") filesToLoad.push(loadScript(file));
-
-      return filesToLoad;
-    }, [])
-  );
+  Promise.all(files.map((file) => loadScript(file)));
 
 export const pxToNum = (value: string | number = 0): number =>
   Number(stripUnit(value));
