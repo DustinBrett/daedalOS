6029b7f384648abdf9cfefdd0170e8f274185796
Dustin Brett <dustinbrett@gmail.com>
2021-07-10 22:47:12 -0700

    User select nothing

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -9,12 +9,12 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     outline: 0;
     padding: 0;
+    user-select: none;
   }
 
   body {
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     overflow: hidden;
-    user-select: none;
   }
 
   ol,
