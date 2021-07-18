40aaf0d1385cee85d37f8fbd42db5b852a6bb1d7
Dustin Brett <dustinbrett@gmail.com>
2021-07-17 21:47:42 -0700

    Created generic "Container" component

---
+++ utils/ContainerComponent.tsx
@@ -0,0 +1,26 @@
+import { useProcesses } from "contexts/process";
+import { useRef } from "react";
+import type { DefaultTheme, StyledComponent } from "styled-components";
+
+type ContainerHook = (
+  id: string,
+  url: string,
+  container: React.MutableRefObject<HTMLDivElement | null>
+) => void;
+
+const ContainerComponent = (
+  id: string,
+  useHook: ContainerHook,
+  Component: StyledComponent<"div", DefaultTheme>
+): JSX.Element => {
+  const {
+    processes: { [id]: { url = "" } = {} },
+  } = useProcesses();
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useHook(id, url, containerRef);
+
+  return <Component ref={containerRef} />;
+};
+
+export default ContainerComponent;
