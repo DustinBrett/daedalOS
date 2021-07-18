6a4e82e8b7216e5a8be1583f9dc48102423e3147
Dustin Brett <dustinbrett@gmail.com>
2021-07-17 22:29:26 -0700

    Keep context menu within desktop bounds

--- components/system/Menu/index.tsx
+++ components/system/Menu/index.tsx
@@ -2,7 +2,10 @@ import MenuItemEntry from "components/system/Menu/MenuItemEntry";
 import StyledMenu from "components/system/Menu/StyledMenu";
 import { useMenu } from "contexts/menu/index";
 import type { MenuState } from "contexts/menu/useMenuContextState";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import type { Position } from "react-rnd";
+import { useTheme } from "styled-components";
+import { pxToNum } from "utils/functions";
 
 type MenuProps = {
   subMenu?: MenuState;
@@ -11,6 +14,10 @@ type MenuProps = {
 const Menu = ({ subMenu }: MenuProps): JSX.Element => {
   const { menu: baseMenu = {}, setMenu } = useMenu();
   const { items, x = 0, y = 0 } = subMenu || baseMenu;
+  const [offset, setOffset] = useState<Position>({ x: 0, y: 0 });
+  const {
+    sizes: { taskbar },
+  } = useTheme();
   const menuRef = useRef<HTMLElement | null>(null);
   const resetMenu = ({
     relatedTarget,
@@ -24,14 +31,27 @@ const Menu = ({ subMenu }: MenuProps): JSX.Element => {
     if (items && !subMenu) menuRef?.current?.focus();
   }, [items, subMenu]);
 
+  useEffect(() => {
+    const { height = 0, width = 0 } =
+      menuRef.current?.getBoundingClientRect() || {};
+    const { innerHeight, innerWidth } = window;
+
+    setOffset({
+      x: Math.round(Math.max(0, x + width - innerWidth)),
+      y: Math.round(
+        Math.max(0, y + height - (innerHeight - pxToNum(taskbar.height)))
+      ),
+    });
+  }, [taskbar.height, x, y]);
+
   return items ? (
     <StyledMenu
       onBlurCapture={resetMenu}
       ref={menuRef}
       isSubMenu={!!subMenu}
       tabIndex={-1}
-      x={x}
-      y={y}
+      x={x - offset.x}
+      y={y - offset.y}
     >
       <ol>
         {items.map((item) => (
