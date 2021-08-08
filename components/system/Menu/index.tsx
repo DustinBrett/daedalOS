7b37901c4ec15e31757875885e4f14305742b086
Dustin Brett <dustinbrett@gmail.com>
2021-08-07 21:43:25 -0700

    Maintain focus on element when context opens

--- components/system/Menu/index.tsx
+++ components/system/Menu/index.tsx
@@ -3,9 +3,9 @@ import menuTransition from "components/system/Menu/menuTransition";
 import StyledMenu from "components/system/Menu/StyledMenu";
 import { useMenu } from "contexts/menu/index";
 import type { MenuState } from "contexts/menu/useMenuContextState";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import type { Position } from "react-rnd";
-import { FOCUSABLE_ELEMENT } from "utils/constants";
+import { FOCUSABLE_ELEMENT, ONE_TIME_PASSIVE_EVENT } from "utils/constants";
 
 type MenuProps = {
   subMenu?: MenuState;
@@ -16,20 +16,46 @@ const Menu = ({ subMenu }: MenuProps): JSX.Element => {
   const { items, x = 0, y = 0 } = subMenu || baseMenu;
   const [offset, setOffset] = useState<Position>({ x: 0, y: 0 });
   const menuRef = useRef<HTMLElement | null>(null);
-  const resetMenu = ({
-    relatedTarget,
-  }: Partial<FocusEvent | MouseEvent> = {}) => {
-    if (
-      !(relatedTarget instanceof HTMLElement) ||
-      !menuRef.current?.contains(relatedTarget)
-    ) {
-      setMenu({});
-    }
-  };
+  const resetMenu = useCallback(
+    ({ relatedTarget }: Partial<FocusEvent | MouseEvent> = {}) => {
+      if (
+        !(relatedTarget instanceof HTMLElement) ||
+        !menuRef.current?.contains(relatedTarget)
+      ) {
+        setMenu({});
+      }
+    },
+    [setMenu]
+  );
 
   useEffect(() => {
-    if (items && !subMenu) menuRef?.current?.focus();
-  }, [items, subMenu]);
+    if (items && !subMenu) {
+      const focusedElement = document.activeElement;
+
+      if (
+        focusedElement instanceof HTMLElement &&
+        focusedElement !== document.body
+      ) {
+        const options: AddEventListenerOptions = {
+          capture: true,
+          ...ONE_TIME_PASSIVE_EVENT,
+        };
+
+        const menuUnfocused: EventListener = ({ type }) => {
+          resetMenu();
+          focusedElement.removeEventListener(
+            type === "click" ? "blur" : "click",
+            menuUnfocused
+          );
+        };
+
+        focusedElement.addEventListener("click", menuUnfocused, options);
+        focusedElement.addEventListener("blur", menuUnfocused, options);
+      } else {
+        menuRef.current?.focus();
+      }
+    }
+  }, [items, resetMenu, subMenu]);
 
   useEffect(() => {
     const { height = 0, width = 0 } =
