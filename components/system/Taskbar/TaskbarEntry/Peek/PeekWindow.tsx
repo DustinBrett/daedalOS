8c26a52050dec7b4ade52c0fd36d4e83a3d60379
Dustin Brett <dustinbrett@gmail.com>
2021-08-14 22:01:20 -0700

    Offset peek at sides

--- components/system/Taskbar/TaskbarEntry/Peek/PeekWindow.tsx
+++ components/system/Taskbar/TaskbarEntry/Peek/PeekWindow.tsx
@@ -5,6 +5,7 @@ import useWindowActions from "components/system/Window/Titlebar/useWindowActions
 import { CloseIcon } from "components/system/Window/Titlebar/WindowActionIcons";
 import { useProcesses } from "contexts/process";
 import { useSession } from "contexts/session";
+import { useEffect, useRef, useState } from "react";
 import Button from "styles/common/Button";
 import { FOCUSABLE_ELEMENT } from "utils/constants";
 
@@ -19,17 +20,34 @@ const PeekWindow = ({ id }: PeekWindowProps): JSX.Element => {
   } = useProcesses();
   const { setForegroundId } = useSession();
   const { onClose } = useWindowActions(id);
+  const [offsetX, setOffsetX] = useState(0);
   const image = useWindowPeek(id);
   const peekTransition = usePeekTransition();
+  const peekRef = useRef<HTMLDivElement | null>(null);
   const onClick = () => {
     if (minimized) minimize(id);
 
     setForegroundId(id);
   };
 
+  useEffect(() => {
+    if (image) {
+      const { left = 0, right = 0 } =
+        peekRef.current?.getBoundingClientRect() || {};
+
+      if (left < 0) {
+        setOffsetX(Math.abs(left));
+      } else if (right > window.innerWidth) {
+        setOffsetX(window.innerWidth - right);
+      }
+    }
+  }, [image]);
+
   return image ? (
     <StyledPeekWindow
       onClick={onClick}
+      ref={peekRef}
+      style={offsetX ? { transform: `translateX(${offsetX}px)` } : undefined}
       {...peekTransition}
       {...FOCUSABLE_ELEMENT}
     >
