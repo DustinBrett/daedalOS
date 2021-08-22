3af180a001aa26812d0308527a7a50b18015d3bb
Dustin Brett <dustinbrett@gmail.com>
2021-08-21 21:40:28 -0700

    Move document imports into pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,30 @@
 import type { DocumentContext, DocumentInitialProps } from "next/document";
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import withStyledComponents from "styles/withStyledComponents";
+import { ServerStyleSheet } from "styled-components";
 import { DEFAULT_LOCALE } from "utils/constants";
+
+const withStyledComponents = async (
+  ctx: DocumentContext
+): Promise<DocumentInitialProps> => {
+  const { renderPage } = ctx;
+  const sheet = new ServerStyleSheet();
+
+  try {
+    ctx.renderPage = () =>
+      renderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const { styles, ...initialProps } = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [styles, sheet.getStyleElement()],
+    };
+  } finally {
+    sheet.seal();
+  }
+};
 
 class MyDocument extends Document {
   public static async getInitialProps(
