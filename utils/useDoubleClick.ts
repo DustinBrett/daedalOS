15c397017ba664042c83091539ebb25eae3d90b4
Dustin Brett <dustinbrett@gmail.com>
2021-08-28 21:29:10 -0700

    Near exact double clicking

--- utils/useDoubleClick.ts
+++ utils/useDoubleClick.ts
@@ -1,20 +1,31 @@
 import { useRef } from "react";
 import { TRANSITIONS_IN_MILLISECONDS } from "utils/constants";
 
+const MAX_MOVES = 5;
+
 const useDoubleClick = (
   handler: React.MouseEventHandler,
   singleClick = false
 ): { onClick: React.MouseEventHandler } => {
   const timer = useRef<NodeJS.Timeout | undefined>();
+  const moveCount = useRef(1);
   const onClick: React.MouseEventHandler = (event) => {
     const runHandler = (): void => {
-      event?.stopPropagation();
+      event.stopPropagation();
       handler(event);
     };
     const clearTimer = (): void => {
       if (timer.current) {
         clearTimeout(timer.current);
         timer.current = undefined;
+      }
+    };
+    const clearWhenPointerMoved = (): void => {
+      if (moveCount.current > MAX_MOVES) {
+        clearTimer();
+        event.target.removeEventListener("pointermove", clearWhenPointerMoved);
+      } else {
+        moveCount.current += 1;
       }
     };
 
@@ -25,6 +36,9 @@ const useDoubleClick = (
         clearTimer,
         TRANSITIONS_IN_MILLISECONDS.DOUBLE_CLICK
       );
+      event.target.addEventListener("pointermove", clearWhenPointerMoved, {
+        passive: true,
+      });
     } else {
       clearTimer();
       runHandler();
