5fe390ab08653b633a62a5229f56b47de114da85
Dustin Brett <dustinbrett@gmail.com>
2021-09-04 22:03:21 -0700

    Clean up monaco (a bit)

--- components/apps/MonacoEditor/index.tsx
+++ components/apps/MonacoEditor/index.tsx
@@ -5,9 +5,13 @@ import type { ComponentProcessProps } from "components/system/Apps/RenderCompone
 import useTitle from "components/system/Window/useTitle";
 import { useFileSystem } from "contexts/fileSystem";
 import { useProcesses } from "contexts/process";
+import type * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
 import { basename } from "path";
 import { useEffect, useState } from "react";
 import { EMPTY_BUFFER } from "utils/constants";
+import { cleanUpGlobals } from "utils/functions";
+
+type IStandaloneCodeEditor = Monaco.editor.IStandaloneCodeEditor;
 
 const MonacoEditor = ({ id }: ComponentProcessProps): JSX.Element => {
   const {
@@ -16,6 +20,7 @@ const MonacoEditor = ({ id }: ComponentProcessProps): JSX.Element => {
   const { fs } = useFileSystem();
   const { appendFileToTitle } = useTitle(id);
   const [value, setValue] = useState("");
+  const [editor, setEditor] = useState<IStandaloneCodeEditor>();
 
   useEffect(() => {
     fs?.readFile(url, (error, contents = EMPTY_BUFFER) => {
@@ -24,13 +29,26 @@ const MonacoEditor = ({ id }: ComponentProcessProps): JSX.Element => {
         appendFileToTitle(basename(url));
       }
     });
-  }, [appendFileToTitle, fs, url]);
+
+    return () => {
+      if (editor) {
+        editor.getModel()?.dispose();
+        editor.dispose();
+        cleanUpGlobals(["monaco"]);
+      }
+    };
+  }, [appendFileToTitle, editor, fs, url]);
 
   loader.config({ paths: { vs: "/libs/monaco/vs" } });
 
   return (
     <StyledMonacoEditor onBlur={overrideSubMenuStyling}>
-      <Editor path={url} theme="vs-dark" value={value} />
+      <Editor
+        onMount={(mountedEditor) => setEditor(mountedEditor)}
+        path={url}
+        theme="vs-dark"
+        value={value}
+      />
     </StyledMonacoEditor>
   );
 };
