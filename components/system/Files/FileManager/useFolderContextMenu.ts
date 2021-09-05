208dbc386ecbe36786b85a207f92a73c887da120
Dustin Brett <dustinbrett@gmail.com>
2021-09-04 22:24:50 -0700

    Add New RTF menu entry

--- components/system/Files/FileManager/useFolderContextMenu.ts
+++ components/system/Files/FileManager/useFolderContextMenu.ts
@@ -6,6 +6,7 @@ import { EMPTY_BUFFER, MENU_SEPERATOR } from "utils/constants";
 
 const NEW_FOLDER = "New folder";
 const NEW_TEXT_DOCUMENT = "New Text Document.txt";
+const NEW_RTF_DOCUMENT = "New Rich Text Document.whtml";
 
 const useFolderContextMenu = (
   url: string,
@@ -31,6 +32,10 @@ const useFolderContextMenu = (
           action: () => newPath(NEW_FOLDER, undefined, true),
         },
         MENU_SEPERATOR,
+        {
+          label: "Rich Text Document",
+          action: () => newPath(NEW_RTF_DOCUMENT, EMPTY_BUFFER, true),
+        },
         {
           label: "Text Document",
           action: () => newPath(NEW_TEXT_DOCUMENT, EMPTY_BUFFER, true),
