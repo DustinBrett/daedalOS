632b1b262c40446c92fcbd2505f1ee27eec46fc5
Dustin Brett <dustinbrett@gmail.com>
2021-09-11 22:13:29 -0700

    Use only basename for title or My PC

--- components/apps/FileExplorer.tsx
+++ components/apps/FileExplorer.tsx
@@ -1,6 +1,7 @@
 import type { ComponentProcessProps } from "components/system/Apps/RenderComponent";
 import FileManager from "components/system/Files/FileManager";
 import { useProcesses } from "contexts/process";
+import { basename } from "path";
 import { useEffect } from "react";
 
 const FileExplorer = ({ id }: ComponentProcessProps): JSX.Element => {
@@ -10,7 +11,7 @@ const FileExplorer = ({ id }: ComponentProcessProps): JSX.Element => {
   } = useProcesses();
 
   useEffect(() => {
-    if (url) title(id, url);
+    if (url) title(id, basename(url) || "My PC");
   }, [id, url, title]);
 
   return url ? <FileManager closing={closing} url={url} view="icon" /> : <></>;
