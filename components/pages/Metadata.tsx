11cf526ff93ed701af2d2e39d1d3a3f8009900ca
Dustin Brett <dustinbrett@gmail.com>
2021-09-18 21:23:49 -0700

    Set viewport scale to 1

--- components/pages/Metadata.tsx
+++ components/pages/Metadata.tsx
@@ -5,6 +5,10 @@ const { description, name } = packageJson;
 
 const Metadata = (): JSX.Element => (
   <Head>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1"
+    />
     <meta name="description" content={description} />
     <title>{name}</title>
   </Head>
