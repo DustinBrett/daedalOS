058e46bbe5dc024b73230007f7b489e924a92d6a
Dustin Brett <dustinbrett@gmail.com>
2021-10-02 21:44:48 -0700

    Clean any possible models

--- components/apps/MonacoEditor/useMonaco.ts
+++ components/apps/MonacoEditor/useMonaco.ts
@@ -37,8 +37,8 @@ const useMonaco = (
     }
 
     return () => {
-      if (editor) {
-        editor.getModel()?.dispose();
+      if (editor && monaco) {
+        monaco.editor.getModels().forEach((model) => model.dispose());
         editor.dispose();
         cleanUpGlobals(globals);
       }
