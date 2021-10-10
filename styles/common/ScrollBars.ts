d11cc4021451f4f8732825aee13e0b31ca981335
Dustin Brett <dustinbrett@gmail.com>
2021-10-09 22:49:15 -0700

    Improved arrows for scrollbars

--- styles/common/ScrollBars.ts
+++ styles/common/ScrollBars.ts
@@ -1,22 +1,5 @@
 import { css } from "styled-components";
 
-const arrowPaths = {
-  down: "M28.109 5.453l3.781 3.766L15.999 25.11.108 9.219l3.781-3.766 12.109 12.109L28.107 5.453z",
-  left: "M26.547 3.891l-12.109 12.109 12.109 12.109-3.766 3.781-15.891-15.891 15.891-15.891 3.766 3.781z",
-  right:
-    "M9.219 0.109l15.891 15.891-15.891 15.891-3.766-3.781 12.109-12.109-12.109-12.109 3.766-3.781z",
-  up: "M16 6.891l15.891 15.891-3.781 3.766-12.109-12.109L3.892 26.548.111 22.782 16.002 6.891z",
-};
-
-const createArrow = (
-  direction: "down" | "left" | "right" | "up",
-  color: string
-): string =>
-  `"data:image/svg+xml;utf8,
-  <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='${color}'>
-    <path d='${arrowPaths[direction]}'/>
-  </svg>"`;
-
 const ScrollBars = css`
   overflow: auto;
 
@@ -73,46 +56,45 @@ const ScrollBars = css`
     }
   }
 
-  ::-webkit-scrollbar-button:single-button:vertical:decrement {
-    background-image: url(${createArrow("up", "dimgrey")});
+  ::-webkit-scrollbar-button:single-button:vertical:decrement,
+  ::-webkit-scrollbar-button:single-button:vertical:increment {
+    background-position: center;
+    background-size: 16px;
     border-bottom: 0;
     border-top: 0;
+    image-rendering: pixelated;
+  }
+
+  ::-webkit-scrollbar-button:single-button:vertical:decrement {
+    background-image: url("/System/Icons/16x16/up.png");
   }
 
   ::-webkit-scrollbar-button:single-button:vertical:increment {
-    background-image: url(${createArrow("down", "dimgrey")});
-    border-bottom: 0;
-    border-top: 0;
+    background-image: url("/System/Icons/16x16/down.png");
+  }
+
+  ::-webkit-scrollbar-button:single-button:horizontal:decrement,
+  ::-webkit-scrollbar-button:single-button:horizontal:increment {
+    background-position: center;
+    background-size: 16px;
+    border-left: 0;
+    border-right: 0;
+    image-rendering: pixelated;
   }
 
   ::-webkit-scrollbar-button:single-button:horizontal:decrement {
-    background-image: url(${createArrow("left", "dimgrey")});
-    background-position: 3px center;
-    border-left: 0;
-    border-right: 0;
+    background-image: url("/System/Icons/16x16/left.png");
   }
 
   ::-webkit-scrollbar-button:single-button:horizontal:increment {
-    background-image: url(${createArrow("right", "dimgrey")});
-    background-position: 4px center;
-    border-left: 0;
-    border-right: 0;
+    background-image: url("/System/Icons/16x16/right.png");
   }
 
-  ::-webkit-scrollbar-button:single-button:vertical:decrement:active {
-    background-image: url(${createArrow("up", "black")});
-  }
-
-  ::-webkit-scrollbar-button:single-button:vertical:increment:active {
-    background-image: url(${createArrow("down", "black")});
-  }
-
-  ::-webkit-scrollbar-button:single-button:horizontal:decrement:active {
-    background-image: url(${createArrow("left", "black")});
-  }
-
+  ::-webkit-scrollbar-button:single-button:vertical:decrement:active,
+  ::-webkit-scrollbar-button:single-button:vertical:increment:active,
+  ::-webkit-scrollbar-button:single-button:horizontal:decrement:active,
   ::-webkit-scrollbar-button:single-button:horizontal:increment:active {
-    background-image: url(${createArrow("right", "black")});
+    background-blend-mode: color-burn;
   }
 `;
 
