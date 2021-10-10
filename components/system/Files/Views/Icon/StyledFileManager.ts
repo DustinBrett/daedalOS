63a2de655fa63ec2922fdce7c523b61893a2a5e2
Dustin Brett <dustinbrett@gmail.com>
2021-10-09 22:04:21 -0700

    Allow overflow out of viewport on desktop

--- components/system/Files/Views/Icon/StyledFileManager.ts
+++ components/system/Files/Views/Icon/StyledFileManager.ts
@@ -21,6 +21,7 @@ const StyledFileManager = styled.ol<StyledFileManagerProps>`
 
   main > & {
     height: ${({ theme }) => `calc(100% - ${theme.sizes.taskbar.height})`};
+    overflow: visible;
     padding-bottom: 21px;
   }
 
