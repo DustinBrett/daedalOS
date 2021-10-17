0f34d70dc20d42228a217f9c337cd046e4fa3ad8
Dustin Brett <dustinbrett@gmail.com>
2021-10-16 22:30:01 -0700

    No snapshots tests for now

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -1,13 +1,13 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import StyledApp from "components/pages/StyledApp";
 import Index from "pages/index";
 
 test("renders main role", () => {
-  const { container } = render(
+  render(
     <StyledApp>
       <Index />
     </StyledApp>
   );
 
-  expect(container).toMatchSnapshot();
+  expect(screen.getByRole("main")).toBeInTheDocument();
 });
