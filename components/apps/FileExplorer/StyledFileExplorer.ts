6017315ff51960b0e8de5cc9acd29b28464c4b4f
Dustin Brett <dustinbrett@gmail.com>
2021-10-30 23:18:22 -0700

    Adjust entry styling for file explorer

--- components/apps/FileExplorer/StyledFileExplorer.ts
+++ components/apps/FileExplorer/StyledFileExplorer.ts
@@ -1,9 +1,19 @@
+import StyledFileEntry from "components/system/Files/Views/Icon/StyledFileEntry";
 import StyledFileManager from "components/system/Files/Views/Icon/StyledFileManager";
 import styled from "styled-components";
 
 const StyledFileExplorer = styled.div`
   ${StyledFileManager} {
+    column-gap: 2px;
     height: calc(100% - 42px - 23px);
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+
+  ${StyledFileEntry} {
+    &::before {
+      border: 0 !important;
+    }
   }
 `;
 
