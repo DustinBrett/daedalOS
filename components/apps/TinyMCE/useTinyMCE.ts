3752382928e6a7a52b8ab6025fdb5add95aeffd0
Dustin Brett <dustinbrett@gmail.com>
2021-10-30 22:13:25 -0700

    Intercept blog clicks to use apps when possible

--- components/apps/TinyMCE/useTinyMCE.ts
+++ components/apps/TinyMCE/useTinyMCE.ts
@@ -1,9 +1,11 @@
 import { config, libs } from "components/apps/TinyMCE/config";
 import { setReadOnlyMode } from "components/apps/TinyMCE/functions";
+import { getProcessByFileExtension } from "components/system/Files/FileEntry/functions";
 import useFileDrop from "components/system/Files/FileManager/useFileDrop";
 import useTitle from "components/system/Window/useTitle";
 import { useFileSystem } from "contexts/fileSystem";
-import { basename, extname } from "path";
+import { useProcesses } from "contexts/process";
+import { basename, extname, relative } from "path";
 import { useCallback, useEffect, useState } from "react";
 import type { Editor, NotificationSpec } from "tinymce";
 import { loadFiles } from "utils/functions";
@@ -14,6 +16,7 @@ const useTinyMCE = (
   containerRef: React.MutableRefObject<HTMLDivElement | null>,
   setLoading: React.Dispatch<React.SetStateAction<boolean>>
 ): void => {
+  const { open } = useProcesses();
   const [editor, setEditor] = useState<Editor>();
   const { appendFileToTitle } = useTitle(id);
   const { readFile, writeFile } = useFileSystem();
@@ -38,6 +41,30 @@ const useTinyMCE = (
     },
     [url, writeFile]
   );
+  const linksToProcesses = useCallback(() => {
+    const iframe = containerRef.current?.querySelector("iframe");
+
+    if (iframe?.contentWindow) {
+      [...iframe.contentWindow.document.links].forEach((link) =>
+        link.addEventListener("click", (event) => {
+          const isRelative =
+            // eslint-disable-next-line dot-notation
+            relative(link.dataset["mceHref"] || "", link.pathname) === "";
+
+          if (isRelative) {
+            event.stopPropagation();
+            event.preventDefault();
+
+            const defaultProcess = getProcessByFileExtension(
+              extname(link.pathname)
+            );
+
+            if (defaultProcess) open(defaultProcess, link.pathname);
+          }
+        })
+      );
+    }
+  }, [containerRef, open]);
   const loadFile = useCallback(async () => {
     if (editor) {
       const fileContents = await readFile(url);
@@ -48,9 +75,11 @@ const useTinyMCE = (
       // eslint-disable-next-line dot-notation
       editor.settings["save_onsavecallback"] = onSave;
 
+      linksToProcesses();
+
       appendFileToTitle(basename(url, extname(url)));
     }
-  }, [appendFileToTitle, editor, onSave, readFile, url]);
+  }, [appendFileToTitle, editor, linksToProcesses, onSave, readFile, url]);
 
   useEffect(() => {
     if (!editor) {
