1fa652d956ff1c6b55fa8ad0a0f1a62567ec66ee
Dustin Brett <dustinbrett@gmail.com>
2021-12-11 22:16:22 -0800

    Avoid Icon re-render on clicking Window

--- components/system/Window/Titlebar/useWindowActions.ts
+++ components/system/Window/Titlebar/useWindowActions.ts
@@ -1,6 +1,7 @@
 import useNextFocusable from "components/system/Window/useNextFocusable";
 import { useProcesses } from "contexts/process";
 import { useSession } from "contexts/session";
+import { useCallback } from "react";
 
 type WindowActions = {
   onClose: () => void;
@@ -17,11 +18,17 @@ const useWindowActions = (id: string): WindowActions => {
     setForegroundId(nextFocusableId);
   };
   const onMaximize = (): void => maximize(id);
-  const onClose = (): void => {
+  const onClose = useCallback((): void => {
     removeFromStack(id);
     closeWithTransition(id);
     setForegroundId(nextFocusableId);
-  };
+  }, [
+    closeWithTransition,
+    id,
+    nextFocusableId,
+    removeFromStack,
+    setForegroundId,
+  ]);
 
   return { onClose, onMaximize, onMinimize };
 };
--- utils/useDoubleClick.ts
+++ utils/useDoubleClick.ts
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 import { TRANSITIONS_IN_MILLISECONDS } from "utils/constants";
 
 const MAX_MOVES = 5;
@@ -9,45 +9,51 @@ const useDoubleClick = (
 ): { onClick: React.MouseEventHandler } => {
   const timer = useRef<number | undefined>();
   const moveCount = useRef(0);
-  const onClick: React.MouseEventHandler = (event) => {
-    const runHandler = (): void => {
-      event.stopPropagation();
-      handler(event);
-    };
-    const clearTimer = (): void => {
-      if (timer.current) {
-        clearTimeout(timer.current);
-        timer.current = undefined;
-      }
-    };
-    const clearWhenPointerMoved = (): void => {
-      if (moveCount.current >= MAX_MOVES) {
-        clearTimer();
-      }
+  const onClick: React.MouseEventHandler = useCallback(
+    (event) => {
+      const runHandler = (): void => {
+        event.stopPropagation();
+        handler(event);
+      };
+      const clearTimer = (): void => {
+        if (timer.current) {
+          clearTimeout(timer.current);
+          timer.current = undefined;
+        }
+      };
+      const clearWhenPointerMoved = (): void => {
+        if (moveCount.current >= MAX_MOVES) {
+          clearTimer();
+        }
 
-      if (typeof timer.current === "undefined") {
-        event.target.removeEventListener("pointermove", clearWhenPointerMoved);
-        moveCount.current = 0;
+        if (typeof timer.current === "undefined") {
+          event.target.removeEventListener(
+            "pointermove",
+            clearWhenPointerMoved
+          );
+          moveCount.current = 0;
+        } else {
+          moveCount.current += 1;
+        }
+      };
+
+      if (singleClick) {
+        runHandler();
+      } else if (typeof timer.current === "undefined") {
+        timer.current = window.setTimeout(
+          clearTimer,
+          TRANSITIONS_IN_MILLISECONDS.DOUBLE_CLICK
+        );
+        event.target.addEventListener("pointermove", clearWhenPointerMoved, {
+          passive: true,
+        });
       } else {
-        moveCount.current += 1;
+        clearTimer();
+        runHandler();
       }
-    };
-
-    if (singleClick) {
-      runHandler();
-    } else if (typeof timer.current === "undefined") {
-      timer.current = window.setTimeout(
-        clearTimer,
-        TRANSITIONS_IN_MILLISECONDS.DOUBLE_CLICK
-      );
-      event.target.addEventListener("pointermove", clearWhenPointerMoved, {
-        passive: true,
-      });
-    } else {
-      clearTimer();
-      runHandler();
-    }
-  };
+    },
+    [handler, singleClick]
+  );
 
   return { onClick };
 };
