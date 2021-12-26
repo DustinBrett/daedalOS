bbd677d6fe267d4b0ec231b8312fc4a4931f75d6
Dustin Brett <dustinbrett@gmail.com>
2021-12-25 21:34:32 -0800

    Ruffle placeholder text

--- components/apps/Ruffle/StyledRuffle.ts
+++ components/apps/Ruffle/StyledRuffle.ts
@@ -4,6 +4,21 @@ const StyledRuffle = styled.div`
   height: 100%;
   width: 100%;
 
+  &:before {
+    color: #ffad33;
+    content: "Drop an SWF/SPL file";
+    position: absolute;
+    font-size: 16px;
+    font-weight: 600;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    place-content: center;
+    place-items: center;
+    display: flex;
+  }
+
   ruffle-player {
     height: 100%;
     width: 100%;
