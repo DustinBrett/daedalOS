49533ce0dc0e703e1ed9e942c18f4fb2f876d58e
Dustin Brett <dustinbrett@gmail.com>
2021-12-30 13:22:41 -0800

    Detect default app in url

--- hooks/useUrlLoader.ts
+++ hooks/useUrlLoader.ts
@@ -1,4 +1,8 @@
+import type { ExtensionType } from "components/system/Files/FileEntry/extensions";
+import extensions from "components/system/Files/FileEntry/extensions";
+import { getDefaultFileViewer } from "components/system/Files/FileEntry/functions";
 import { useProcesses } from "contexts/process";
+import { extname } from "path";
 import { useEffect } from "react";
 
 const useUrlLoader = (): void => {
@@ -6,10 +10,18 @@ const useUrlLoader = (): void => {
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
-    const app = searchParams.get("app");
+    const app = searchParams.get("app") || "";
     const url = searchParams.get("url") || "";
 
     if (app) open(app, { url });
+    else if (url) {
+      const extension = extname(url);
+      let { process: [defaultApp] = [] } =
+        extensions[extension as ExtensionType] || {};
+
+      if (!defaultApp) defaultApp = getDefaultFileViewer(extension);
+      if (defaultApp) open(defaultApp, { url });
+    }
   }, [open]);
 };
 
