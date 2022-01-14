6051e4af2e84c9897deac08bfb2b5e153e87eb9d
Dustin Brett <dustinbrett@gmail.com>
2022-01-13 23:24:33 -0800

    More window size adjustments for consistency/ratio

--- contexts/process/directory.ts
+++ contexts/process/directory.ts
@@ -20,7 +20,7 @@ const processDirectory: Processes = {
     background: "#FFF",
     defaultSize: {
       height: 480,
-      width: 600,
+      width: 640,
     },
     icon: "/System/Icons/chromium.png",
     title: "Browser",
@@ -69,8 +69,8 @@ const processDirectory: Processes = {
     Component: dynamic(() => import("components/apps/MonacoEditor")),
     background: "#1E1E1E",
     defaultSize: {
-      height: 477,
-      width: 530,
+      height: 480,
+      width: 544,
     },
     icon: "/System/Icons/monaco.png",
     title: "Monaco Editor",
@@ -79,8 +79,8 @@ const processDirectory: Processes = {
     Component: dynamic(() => import("components/apps/PDF")),
     background: "rgb(82, 86, 89)",
     defaultSize: {
-      height: 493,
-      width: 637,
+      height: 480,
+      width: 640,
     },
     icon: "/System/Icons/pdf.png",
     title: "PDF",
