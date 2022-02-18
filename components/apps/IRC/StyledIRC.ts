4e890e593758832245fcf5ca76488bc86c9fcd96
Dustin Brett <dustinbrett@gmail.com>
2022-02-17 20:01:22 -0800

    Lint/pkg upgrades

--- components/apps/IRC/StyledIRC.ts
+++ components/apps/IRC/StyledIRC.ts
@@ -30,13 +30,13 @@ const StyledIRC = styled.div`
   }
 
   textarea {
-    border-top: 1px solid #e1e1e1;
     border-bottom: 1px solid #e1e1e1;
+    border-top: 1px solid #e1e1e1;
     height: 100%;
     resize: none;
   }
 
-  input {
+  input > {
     height: 25px;
   }
 `;
