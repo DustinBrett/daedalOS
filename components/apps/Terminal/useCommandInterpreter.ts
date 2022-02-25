6a84c5d49fd4084fea227a374d183622d1e4b757
Dustin Brett <dustinbrett@gmail.com>
2022-02-24 20:59:17 -0800

    Update autocomplete on ls/dir

--- components/apps/Terminal/useCommandInterpreter.ts
+++ components/apps/Terminal/useCommandInterpreter.ts
@@ -287,6 +287,7 @@ const useCommandInterpreter = (
               `\t\t${fileCount} File(s) ${totalSize.toLocaleString()} bytes`
             );
             localEcho?.println(`\t\t${directoryCount} Dir(s)`);
+            if (localEcho) autoComplete(entries, localEcho);
           };
 
           if (directory) {
