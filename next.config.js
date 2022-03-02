75c727d58d4148dbb8ee7192d359404e75884055
Dustin Brett <dustinbrett@gmail.com>
2022-03-01 21:02:05 -0800

    Switch to swc compiler, no more babel

--- next.config.js
+++ next.config.js
@@ -4,10 +4,16 @@
  * @type {import('next').NextConfig}
  * */
 const nextConfig = {
+  compiler: {
+    reactRemoveProperties: true,
+    removeConsole: true,
+    styledComponents: true,
+  },
   experimental: {
     reactRoot: true,
   },
   reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
