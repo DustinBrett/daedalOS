bba6d1dd543ce30422e0115b6459818d9e2c803e
Dustin Brett <dustinbrett@gmail.com>
2022-03-05 23:11:19 -0800

    Pkg upgrades/cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = require("next/jest")()({
   moduleDirectories: [".", "node_modules"],
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   testEnvironment: "jsdom",
 });
