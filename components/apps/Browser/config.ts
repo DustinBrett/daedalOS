8f93f75c34c0d514df496ca3402dd3397af6ace7
Dustin Brett <dustinbrett@gmail.com>
2022-03-23 20:22:13 -0700

    Reorganize favicons

--- components/apps/Browser/config.ts
+++ components/apps/Browser/config.ts
@@ -6,7 +6,7 @@ type Bookmark = {
 
 export const bookmarks: Bookmark[] = [
   {
-    icon: "/System/Icons/google.png",
+    icon: "/System/Icons/Favicons/google.png",
     name: "Google",
     url: "https://www.google.com/webhp?igu=1",
   },
@@ -16,27 +16,27 @@ export const bookmarks: Bookmark[] = [
     url: "https://dustinbrett.com/",
   },
   {
-    icon: "/System/Icons/win96.png",
+    icon: "/System/Icons/Favicons/win96.png",
     name: "Windows 96",
     url: "https://windows96.net/",
   },
   {
-    icon: "/System/Icons/aos.png",
+    icon: "/System/Icons/Favicons/aos.png",
     name: "AaronOS",
     url: "https://aaronos.dev/AaronOS/",
   },
   {
-    icon: "/System/Icons/jspaint.png",
+    icon: "/System/Icons/Favicons/jspaint.png",
     name: "JS Paint",
     url: "https://jspaint.app/",
   },
   {
-    icon: "/System/Icons/minecraft.png",
+    icon: "/System/Icons/Favicons/minecraft.png",
     name: "Minecraft Classic",
     url: "https://classic.minecraft.net/",
   },
   {
-    icon: "/System/Icons/diablo.png",
+    icon: "/System/Icons/Favicons/diablo.png",
     name: "Diablo",
     url: "https://d07riv.github.io/diabloweb/",
   },
