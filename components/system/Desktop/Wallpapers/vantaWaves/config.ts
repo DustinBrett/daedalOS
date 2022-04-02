7cbd111d1a6b335cffdc6eacaa108db436a59ae5
Dustin Brett <dustinbrett@gmail.com>
2022-04-01 20:47:06 -0700

    vanta optimizations

--- components/system/Desktop/Wallpapers/vantaWaves/config.ts
+++ components/system/Desktop/Wallpapers/vantaWaves/config.ts
@@ -1,13 +1,25 @@
 export const config = {
+  camera: {
+    far: 400,
+    fov: 30,
+    near: 0.1,
+  },
   color: 0x192b34,
   colorCycleSpeed: 5,
   forceAnimate: true,
+  hh: 50,
   hue: 200,
   lightness: 15,
+  material: {
+    options: {
+      wireframe: false,
+    },
+  },
   saturation: 35,
   shininess: 35,
   waveHeight: 15,
   waveSpeed: 0.25,
+  ww: 50,
 };
 
 export const disableControls = {
