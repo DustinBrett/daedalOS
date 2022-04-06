c66456deb2c4ef95501c5a4bd90459357052d7e8
Dustin Brett <dustinbrett@gmail.com>
2022-04-05 22:09:08 -0700

    Don't flash 0 bytes on directory selection

--- components/system/Files/FileManager/StatusBar.tsx
+++ components/system/Files/FileManager/StatusBar.tsx
@@ -12,10 +12,12 @@ type StatusBarProps = {
 };
 
 const MINIMUM_STATUSBAR_WIDTH = 225;
+const UNKNOWN_SIZE = -1;
+const UNCALCULATED_SIZE = -2;
 
 const StatusBar: FC<StatusBarProps> = ({ count, directory, selected }) => {
   const { exists, lstat, stat } = useFileSystem();
-  const [selectedSize, setSelectedSize] = useState(0);
+  const [selectedSize, setSelectedSize] = useState(UNKNOWN_SIZE);
   const [showSelected, setShowSelected] = useState(false);
   const updateShowSelected = (width: number): void =>
     setShowSelected(width > MINIMUM_STATUSBAR_WIDTH);
@@ -26,18 +28,18 @@ const StatusBar: FC<StatusBarProps> = ({ count, directory, selected }) => {
         await selected.reduce(async (totalSize, file) => {
           const currentSize = await totalSize;
 
-          if (currentSize === -1) return -1;
+          if (currentSize === UNCALCULATED_SIZE) return UNCALCULATED_SIZE;
 
           const path = join(directory, file);
 
           if (await exists(path)) {
             return (await lstat(path)).isDirectory()
-              ? -1
+              ? UNCALCULATED_SIZE
               : currentSize + (await stat(path)).size;
           }
 
           return totalSize;
-        }, Promise.resolve(0))
+        }, Promise.resolve(UNKNOWN_SIZE))
       ),
     [directory, exists, lstat, selected, stat]
   );
@@ -68,7 +70,7 @@ const StatusBar: FC<StatusBarProps> = ({ count, directory, selected }) => {
       {showSelected && selected.length > 0 && (
         <div className="selected" {...label("Selected item count and size")}>
           {selected.length} item{selected.length !== 1 ? "s" : ""} selected
-          {selectedSize > -1
+          {selectedSize !== UNKNOWN_SIZE && selectedSize !== UNCALCULATED_SIZE
             ? `${"\u00A0\u00A0"}${getFormattedSize(selectedSize)}`
             : ""}
         </div>
