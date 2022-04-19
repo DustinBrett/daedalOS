f17eb510b27a7f3a70d7aec239587aa0e8010ec8
Dustin Brett <dustinbrett@gmail.com>
2022-04-19 13:27:32 -0700

    Don't do contain for webamp

--- components/apps/Webamp/StyledWebamp.ts
+++ components/apps/Webamp/StyledWebamp.ts
@@ -2,7 +2,6 @@ import { m as motion } from "framer-motion";
 import styled from "styled-components";
 
 const StyledWebamp = styled(motion.div)`
-  contain: strict;
   inset: 0;
   pointer-events: none;
   position: absolute;
