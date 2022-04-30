26de0d8f7af0d7c182e4e95f8fb3f977f8634827
Dustin Brett <dustinbrett@gmail.com>
2022-04-29 20:01:45 -0700

    Dont show context menu on boxedwine canvas

--- components/apps/BoxedWine/index.tsx
+++ components/apps/BoxedWine/index.tsx
@@ -2,13 +2,14 @@ import ContainerComponent from "components/apps/AppContainer";
 import StyledBoxedWine from "components/apps/BoxedWine/StyledBoxedWine";
 import useBoxedWine from "components/apps/BoxedWine/useBoxedWine";
 import type { ComponentProcessProps } from "components/system/Apps/RenderComponent";
+import { haltEvent } from "utils/functions";
 
 const BoxedWine: FC<ComponentProcessProps> = ({ id }) =>
   ContainerComponent(
     id,
     useBoxedWine,
     StyledBoxedWine,
-    <canvas id="boxedWineCanvas" />
+    <canvas id="boxedWineCanvas" onContextMenu={haltEvent} />
   );
 
 export default BoxedWine;
