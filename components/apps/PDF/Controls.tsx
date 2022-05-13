09a31deabd18945487045b1a01ab37cff6a2250d
Dustin Brett <dustinbrett@gmail.com>
2022-05-12 22:24:08 -0700

    Add control inputs for PDF

--- components/apps/PDF/Controls.tsx
+++ components/apps/PDF/Controls.tsx
@@ -7,14 +7,36 @@ import { label } from "utils/functions";
 
 const Controls: FC<ComponentProcessProps> = ({ id }) => {
   const { argument, processes: { [id]: process } = {} } = useProcesses();
-  const { count = 0, page: currentPage = 1, scale = 1 } = process || {};
+  const {
+    count = 0,
+    page: currentPage = 1,
+    componentWindow,
+    scale = 1,
+  } = process || {};
 
   return (
     <StyledControls>
       <ol>
         {count !== 0 && (
           <li className="pages">
-            <input enterKeyHint="go" value={currentPage} /> / {count}
+            <input
+              enterKeyHint="go"
+              onChange={({ target }) => {
+                const newPage = Number(target.value);
+
+                if (Number.isNaN(newPage) || newPage < 1 || newPage > count) {
+                  return;
+                }
+
+                argument(id, "page", newPage);
+
+                componentWindow
+                  ?.querySelectorAll("li")
+                  [newPage - 1].scrollIntoView();
+              }}
+              value={currentPage}
+            />{" "}
+            / {count}
           </li>
         )}
         <li className="scale">
@@ -30,6 +52,31 @@ const Controls: FC<ComponentProcessProps> = ({ id }) => {
           <input
             disabled={count === 0}
             enterKeyHint="done"
+            onChange={({ target }) => {
+              if (
+                !target.value.endsWith("%") ||
+                target.value.length > 4 ||
+                target.value.length < 2
+              ) {
+                return;
+              }
+
+              const newScale = Number(target.value.replace("%", "")) / 100;
+
+              if (
+                Number.isNaN(newScale) ||
+                newScale > scales[scales.length - 1] ||
+                newScale < scales[0]
+              ) {
+                return;
+              }
+
+              argument(
+                id,
+                "scale",
+                scales[scales.findIndex((s) => s >= newScale)]
+              );
+            }}
             value={`${Math.round(scale * 100)}%`}
           />
 
