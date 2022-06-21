7fd92529af39f7236757c8a7de4f5eff26ffcf18
Dustin Brett <dustinbrett@gmail.com>
2022-06-20 22:22:39 -0700

    Dont use quotes for APOD url

--- components/system/Desktop/Wallpapers/useWallpaper.ts
+++ components/system/Desktop/Wallpapers/useWallpaper.ts
@@ -168,7 +168,7 @@ const useWallpaper = (
       desktopRef.current?.setAttribute(
         "style",
         `
-        background-image: url("${wallpaperUrl}")${
+        background-image: url(${wallpaperUrl})${
           fallbackBackground ? `, url(${fallbackBackground})` : ""
         };
         ${cssFit[newWallpaperFit]}
