c265cd4b20d4751bb37585467851c428861a5d45
Dustin Brett <dustinbrett@gmail.com>
2022-07-30 09:25:54 -0700

    Revert title/icon when leaving tab/page

--- components/pages/Metadata.tsx
+++ components/pages/Metadata.tsx
@@ -19,6 +19,12 @@ const Metadata: FC = () => {
   const [favIcon, setFavIcon] = useState("");
   const { foregroundId } = useSession();
   const { processes: { [foregroundId]: process } = {} } = useProcesses();
+  const resetFaviconAndTitle = (): void => {
+    setTitle(alias);
+    setFavIcon((currentFavicon) =>
+      currentFavicon ? FAVICON_BASE_PATH : currentFavicon
+    );
+  };
 
   useEffect(() => {
     if (process) {
@@ -29,12 +35,30 @@ const Metadata: FC = () => {
         setFavIcon(process.icon || FAVICON_BASE_PATH);
       }
     } else {
-      setTitle(alias);
-      setFavIcon((currentFavicon) =>
-        currentFavicon ? FAVICON_BASE_PATH : currentFavicon
-      );
+      resetFaviconAndTitle();
     }
   }, [process, title]);
+
+  useEffect(() => {
+    const onVisibilityChange = (): void => {
+      if (document.visibilityState === "visible") {
+        resetFaviconAndTitle();
+      }
+    };
+    const visibilityEventName =
+      "webkitHidden" in document
+        ? "webkitvisibilitychange"
+        : "visibilitychange";
+
+    document.addEventListener(visibilityEventName, onVisibilityChange, {
+      passive: true,
+    });
+
+    return document.removeEventListener(
+      visibilityEventName,
+      onVisibilityChange
+    );
+  }, []);
 
   return (
     <Head>
