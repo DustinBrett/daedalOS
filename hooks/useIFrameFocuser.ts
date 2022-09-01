688f85f4039aab9fcc01ca71e8a51d0da92129a0
Dustin Brett <dustinbrett@gmail.com>
2022-08-31 21:16:47 -0700

    Keep iframe focus callback ref updated

--- hooks/useIFrameFocuser.ts
+++ hooks/useIFrameFocuser.ts
@@ -1,11 +1,13 @@
 import { useProcesses } from "contexts/process";
+import type { Processes } from "contexts/process/types";
 import { useSession } from "contexts/session";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { ONE_TIME_PASSIVE_EVENT } from "utils/constants";
 
 const useIFrameFocuser = (): void => {
   const { processes } = useProcesses();
   const { setForegroundId } = useSession();
+  const processesRef = useRef<Processes>({});
   const focusIframeWindow = useCallback((): void => {
     if (document.activeElement instanceof HTMLIFrameElement) {
       const [id] =
@@ -19,7 +21,7 @@ const useIFrameFocuser = (): void => {
           "click",
           ({ target }) => {
             const [focusId = ""] =
-              Object.entries(processes).find(
+              Object.entries(processesRef.current).find(
                 ([, { componentWindow }]) =>
                   target instanceof HTMLElement &&
                   componentWindow?.contains(target)
@@ -38,6 +40,10 @@ const useIFrameFocuser = (): void => {
 
     return () => window.removeEventListener("blur", focusIframeWindow);
   }, [focusIframeWindow]);
+
+  useEffect(() => {
+    processesRef.current = processes;
+  }, [processes]);
 };
 
 export default useIFrameFocuser;
