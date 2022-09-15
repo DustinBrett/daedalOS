9d6ee74be057641fe0426fa95c84606e49363a71
Dustin Brett <dustinbrett@gmail.com>
2022-09-15 10:21:33 -0700

    More visible error component

--- components/system/Apps/ComponentError.tsx
+++ components/system/Apps/ComponentError.tsx
@@ -1,16 +1,17 @@
 import styled from "styled-components";
 
 const StyledComponentError = styled.div`
+  background-color: #fff;
+  color: #000;
   display: flex;
-  font-size: 22px;
+  font-size: 20px;
   height: 100%;
-  mix-blend-mode: difference;
   place-content: center;
   place-items: center;
   width: 100%;
 `;
 
-const ERROR_MESSAGE = "Error occured while loading component.";
+const ERROR_MESSAGE = "Error occured within component.";
 
 const ComponentError: FC = () => (
   <StyledComponentError>{ERROR_MESSAGE}</StyledComponentError>
