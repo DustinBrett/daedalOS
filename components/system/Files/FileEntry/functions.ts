0f4268007f8e3ef7af9cf7b0f832837338a88800
Dustin Brett <dustinbrett@gmail.com>
2022-11-12 20:31:58 -0800

    Fix dynamic cached shortcut icon

--- components/system/Files/FileEntry/functions.ts
+++ components/system/Files/FileEntry/functions.ts
@@ -281,15 +281,27 @@ export const getInfoWithExtension = (
           `${url}${ICON_CACHE_EXTENSION}`
         );
 
-        fs.exists(cachedIconPath, (cachedIconExists) => {
-          if (cachedIconExists) {
-            callback({
-              comment,
-              icon: cachedIconPath,
-              pid,
-              subIcons,
-              url,
-            });
+        fs.lstat(cachedIconPath, (statError, cachedIconStats) => {
+          if (!statError && cachedIconStats) {
+            if (cachedIconStats.birthtimeMs !== cachedIconStats.ctimeMs) {
+              fs.readFile(cachedIconPath, (_readError, cachedIconData) =>
+                callback({
+                  comment,
+                  icon: bufferToUrl(cachedIconData as Buffer),
+                  pid,
+                  subIcons,
+                  url,
+                })
+              );
+            } else {
+              callback({
+                comment,
+                icon: cachedIconPath,
+                pid,
+                subIcons,
+                url,
+              });
+            }
           } else {
             getInfoWithExtension(fs, url, urlExt, (fileInfo) => {
               const {
