7f2a033cb7a8153e55e3406cb68b658ac2c97e74
Dustin Brett <dustinbrett@gmail.com>
2022-12-26 11:35:03 -0800

    Add channel names to IRC on join/part

--- components/apps/IRC/config.ts
+++ components/apps/IRC/config.ts
@@ -1,6 +1,5 @@
 const IRC_SERVERS = [
   ["Libera.Chat", "web.libera.chat/webirc/websocket/"],
-  ["Unreal", "irc.unrealircd.org"],
   ["ergo", "irc.ergo.chat/webirc"],
   ["ErgoTestnet", "testnet.ergo.chat/webirc"],
   ["InspIRCd Testnet", "testnet.inspircd.org", 8097],
--- components/apps/IRC/index.tsx
+++ components/apps/IRC/index.tsx
@@ -3,12 +3,28 @@ import StyledIRC from "components/apps/IRC/StyledIRC";
 import type { ComponentProcessProps } from "components/system/Apps/RenderComponent";
 import StyledLoading from "components/system/Files/FileManager/StyledLoading";
 import { useProcesses } from "contexts/process";
+import processDirectory from "contexts/process/directory";
 import { useEffect, useRef, useState } from "react";
 
+type KiwiIrcClient = {
+  on: (
+    event: "irc.join" | "irc.part",
+    callback: (
+      channelData: {
+        channel: string;
+      },
+      serverData: {
+        name: string;
+      }
+    ) => void
+  ) => void;
+};
+
 const IRC: FC<ComponentProcessProps> = ({ id }) => {
-  const { processes: { [id]: { libs: [ircSrc = ""] = [] } = {} } = {} } =
+  const { processes: { [id]: { libs: [ircSrc = ""] = [] } = {} } = {}, title } =
     useProcesses();
   const [loaded, setLoaded] = useState(false);
+  const [channels, setChannels] = useState<string[]>([]);
   const iframeRef = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
@@ -32,8 +48,38 @@ const IRC: FC<ComponentProcessProps> = ({ id }) => {
         (networkEntryCollapse, index) =>
           index !== 0 && networkEntryCollapse?.click()
       );
+
+      (
+        iframeRef.current.contentWindow as Window & {
+          kiwi: KiwiIrcClient;
+        }
+      )?.kiwi.on("irc.join", ({ channel }, { name }) =>
+        setChannels((currentChannels) => [
+          ...new Set([...currentChannels, `${channel}/${name}`]),
+        ])
+      );
+      (
+        iframeRef.current.contentWindow as Window & {
+          kiwi: KiwiIrcClient;
+        }
+      )?.kiwi.on("irc.part", ({ channel }, { name }) =>
+        setChannels((currentChannels) =>
+          currentChannels.filter(
+            (currentChannel) => currentChannel !== `${channel}/${name}`
+          )
+        )
+      );
     }
   }, [loaded]);
+
+  useEffect(() => {
+    title(
+      id,
+      `${processDirectory.IRC.title}${
+        channels.length === 0 ? "" : ` - ${channels.join(", ")}`
+      }`
+    );
+  }, [channels, id, title]);
 
   return (
     <StyledIRC>
