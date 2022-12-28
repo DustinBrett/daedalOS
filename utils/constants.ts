ae06374fce918974c7c390a10db202144f489fae
Dustin Brett <dustinbrett@gmail.com>
2022-12-28 13:52:36 -0800

    Slightly increase clock width for Linux fonts

--- utils/constants.ts
+++ utils/constants.ts
@@ -29,7 +29,7 @@ export const DEFAULT_WALLPAPER_FIT: WallpaperFit = "fill";
 
 export const THIN_SCROLLBAR_WIDTH = 13;
 
-export const BASE_CLOCK_WIDTH = 66;
+export const BASE_CLOCK_WIDTH = 68;
 
 export const SMALLEST_PNG_SIZE = 51;
 
