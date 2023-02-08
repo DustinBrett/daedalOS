814a0b88bf535015b558b521df0728edfb3f27ff
Dustin Brett <dustinbrett@gmail.com>
2023-02-07 22:19:21 -0800

    Refactored url loader hook

--- hooks/useUrlLoader.ts
+++ hooks/useUrlLoader.ts
@@ -4,46 +4,57 @@ import { useFileSystem } from "contexts/fileSystem";
 import { useProcesses } from "contexts/process";
 import processDirectory from "contexts/process/directory";
 import { extname } from "path";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { getSearchParam } from "utils/functions";
 
 const useUrlLoader = (): void => {
-  const { exists } = useFileSystem();
+  const { exists, fs } = useFileSystem();
   const { open } = useProcesses();
-  const [initialApp, setInitialApp] = useState<string>("");
-  const loadInitialApp = useCallback(async () => {
-    const url = getSearchParam("url");
-    const urlExists =
-      (initialApp === "Browser" && url.startsWith("http")) ||
-      (await exists(url));
-
-    open(initialApp, urlExists ? { url } : undefined);
-  }, [exists, initialApp, open]);
+  const loadedInitialAppRef = useRef(false);
 
   useEffect(() => {
+    if (loadedInitialAppRef.current || !fs || !exists || !open) return;
+
+    loadedInitialAppRef.current = true;
+
     const app = getSearchParam("app");
     const url = getSearchParam("url");
+
+    const loadInitialApp = async (initialApp: string): Promise<void> => {
+      if (!initialApp) return;
+
+      let urlExists = false;
+
+      try {
+        urlExists =
+          (initialApp === "Browser" && url.startsWith("http")) ||
+          (await exists(url));
+      } catch {
+        // Ignore error checking if url exists
+      }
+
+      if (initialApp === "FileExplorer" && url && !urlExists) return;
+
+      open(initialApp, urlExists ? { url } : undefined);
+    };
 
     if (app) {
       const lcAppNames = Object.fromEntries(
         Object.keys(processDirectory).map((name) => [name.toLowerCase(), name])
       );
 
-      setInitialApp(lcAppNames[app.toLowerCase()]);
+      loadInitialApp(lcAppNames[app.toLowerCase()]);
     } else if (url) {
       const extension = extname(url).toLowerCase();
       const { process: [defaultApp] = [] } = extensions[extension] || {};
 
-      setInitialApp(defaultApp || getDefaultFileViewer(extension));
+      loadInitialApp(
+        extension
+          ? defaultApp || getDefaultFileViewer(extension)
+          : "FileExplorer"
+      );
     }
-  }, []);
-
-  useEffect(() => {
-    if (initialApp) {
-      setInitialApp("");
-      loadInitialApp();
-    }
-  }, [initialApp, loadInitialApp]);
+  }, [exists, fs, open]);
 };
 
 export default useUrlLoader;
