ed95f41df621694cada07ce64558297d1f13d3dd
Dustin Brett <dustinbrett@gmail.com>
2023-02-18 19:52:04 -0800

    Improvements in images made code simpler

--- components/system/Files/FileEntry/SubIcons.tsx
+++ components/system/Files/FileEntry/SubIcons.tsx
@@ -1,6 +1,6 @@
 import type { FileManagerViewNames } from "components/system/Files/Views";
 import { FileEntryIconSize } from "components/system/Files/Views";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import Icon from "styles/common/Icon";
 import {
   FOLDER_BACK_ICON,
@@ -25,7 +25,7 @@ type SubIconsProps = IconProps & {
   subIcons?: string[];
 };
 
-const WIDE_IMAGE_TRANSFORM = "matrix(0.5, 0.07, 0, 0.7, 2, 2)";
+const WIDE_IMAGE_TRANSFORM = "matrix(0.5, 0.05, 0, 0.7, 2, 1)";
 const SHORT_IMAGE_TRANSFORM = "matrix(0.4, 0.14, 0, 0.7, -4, 2)";
 
 const SubIcon: FC<SubIconProps> = ({
@@ -46,45 +46,29 @@ const SubIcon: FC<SubIconProps> = ({
       ],
     [icon, view]
   );
-  const [aspectRatio, setAspectRatio] = useState(0);
   const style = useMemo((): React.CSSProperties | undefined => {
     if (icon === FOLDER_FRONT_ICON) return { zIndex: 3 };
 
     if (baseIcon === FOLDER_BACK_ICON) {
       const hasMultipleSubIcons = totalSubIcons - 1 > 1;
-
-      let transform: string;
-
-      if (isFirstImage) {
-        transform = hasMultipleSubIcons
+      const transform = isFirstImage
+        ? hasMultipleSubIcons
           ? SHORT_IMAGE_TRANSFORM
-          : WIDE_IMAGE_TRANSFORM;
-      } else {
-        transform = WIDE_IMAGE_TRANSFORM;
-      }
+          : WIDE_IMAGE_TRANSFORM
+        : WIDE_IMAGE_TRANSFORM;
 
       return {
-        transform: `${transform} translateZ(0px)${
-          aspectRatio > 1.5 ? " rotate(90deg) scaleY(2)" : ""
-        }`,
+        objectFit: "cover",
+        transform: `${transform} translateZ(0px)`,
         zIndex: isFirstImage ? 2 : 1,
       };
     }
 
     return undefined;
-  }, [aspectRatio, baseIcon, icon, isFirstImage, totalSubIcons]);
+  }, [baseIcon, icon, isFirstImage, totalSubIcons]);
 
   return (
     <Icon
-      ref={(iconRef) => {
-        if (iconRef && icon.startsWith("blob:")) {
-          iconRef.addEventListener("load", () => {
-            if (iconRef.naturalWidth && iconRef.naturalHeight) {
-              setAspectRatio(iconRef.naturalWidth / iconRef.naturalHeight);
-            }
-          });
-        }
-      }}
       $eager={icon === SHORTCUT_ICON}
       alt={name}
       src={icon}
