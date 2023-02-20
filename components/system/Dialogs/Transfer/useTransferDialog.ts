1bba5d525067caa9e5cdcf0d90278b357f626a37
Dustin Brett <dustinbrett@gmail.com>
2023-02-19 16:15:46 -0800

    Dont drag a folder shortcut into itself

--- components/system/Dialogs/Transfer/useTransferDialog.ts
+++ components/system/Dialogs/Transfer/useTransferDialog.ts
@@ -1,7 +1,10 @@
+import { getShortcutInfo } from "components/system/Files/FileEntry/functions";
+import { useFileSystem } from "contexts/fileSystem";
 import { useProcesses } from "contexts/process";
 import { useProcessesRef } from "hooks/useProcessesRef";
+import { extname } from "path";
 import { useEffect, useMemo, useRef } from "react";
-import { PROCESS_DELIMITER } from "utils/constants";
+import { PROCESS_DELIMITER, SHORTCUT_EXTENSION } from "utils/constants";
 
 export type FileReaders = [File, string, FileReader][];
 
@@ -17,12 +20,13 @@ type Dialog = {
   openTransferDialog: (
     fileReaders?: FileReaders | ObjectReaders,
     url?: string
-  ) => void;
+  ) => Promise<void>;
 };
 
 const useTransferDialog = (): Dialog => {
   const { argument, open } = useProcesses();
   const processesRef = useProcessesRef();
+  const { readFile } = useFileSystem();
   const getTransferIdCallbackRef =
     useRef<(url: string) => string | undefined>();
 
@@ -36,23 +40,34 @@ const useTransferDialog = (): Dialog => {
   }, [processesRef]);
 
   return useMemo(
-    () =>
-      ({
-        openTransferDialog: (fileReaders, url) => {
-          if (fileReaders?.length === 0) return;
+    () => ({
+      openTransferDialog: async (fileReaders, url) => {
+        if (fileReaders?.length === 0) return;
 
-          if (fileReaders && url) {
-            const currentPid = getTransferIdCallbackRef.current?.(url);
+        if (fileReaders && url) {
+          const currentPid = getTransferIdCallbackRef.current?.(url);
 
-            if (currentPid) {
-              argument(currentPid, "fileReaders", fileReaders);
-            }
-          } else {
-            open("Transfer", { fileReaders, url });
+          if (currentPid) {
+            argument(currentPid, "fileReaders", fileReaders);
           }
-        },
-      } as Dialog),
-    [argument, open]
+        } else {
+          if (fileReaders?.length === 1 && !Array.isArray(fileReaders[0])) {
+            const [{ directory, name }] = fileReaders;
+
+            if (extname(name) === SHORTCUT_EXTENSION) {
+              const { url: shortcutUrl } = getShortcutInfo(
+                await readFile(name)
+              );
+
+              if (shortcutUrl === directory) return;
+            }
+          }
+
+          open("Transfer", { fileReaders, url });
+        }
+      },
+    }),
+    [argument, open, readFile]
   );
 };
 
