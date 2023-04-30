b17a5e7c21736789676b1b7619778862e2784a27
kumavis <aaron@kumavis.me>
2023-04-30 05:17:44 -1000

    lint fix

--- components/apps/NoodJs/config.ts
+++ components/apps/NoodJs/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   referrerPolicy: "no-referrer" as React.HTMLAttributeReferrerPolicy,
-  // allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation 
+  // allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation
   sandbox: "allow-same-origin allow-scripts",
 };
