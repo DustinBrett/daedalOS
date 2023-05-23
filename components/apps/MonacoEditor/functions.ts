0595bf075e465d56630e77051b0d4e65e8c3cbe8
Dustin Brett <dustinbrett@gmail.com>
2023-05-22 21:19:37 -0700

    Allow Monaco menu out in Safari mobile

--- components/apps/MonacoEditor/functions.ts
+++ components/apps/MonacoEditor/functions.ts
@@ -22,13 +22,26 @@ export const detectLanguage = (ext: string): string => {
 export const relocateShadowRoot: React.FocusEventHandler = ({
   relatedTarget,
 }): void => {
-  if (
-    relatedTarget instanceof HTMLElement &&
-    relatedTarget.classList.value === "shadow-root-host" &&
-    relatedTarget.shadowRoot instanceof ShadowRoot &&
-    relatedTarget.closest("section")
-  ) {
-    relatedTarget.closest("section")?.parentNode?.prepend(relatedTarget);
+  if (relatedTarget instanceof HTMLElement) {
+    let targetElement: HTMLElement | undefined;
+
+    if (relatedTarget.classList.value === "actions-container") {
+      targetElement = relatedTarget.closest(
+        ".monaco-menu-container"
+      ) as HTMLElement;
+    } else if (
+      relatedTarget.classList.value === "shadow-root-host" &&
+      relatedTarget.shadowRoot instanceof ShadowRoot
+    ) {
+      targetElement = relatedTarget;
+    }
+
+    if (
+      targetElement instanceof HTMLElement &&
+      targetElement.closest("section")
+    ) {
+      targetElement.closest("section")?.parentNode?.prepend(targetElement);
+    }
   }
 };
 
