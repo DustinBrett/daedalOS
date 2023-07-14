aad14683a5e159b2c4cb83afe5eda250e91fd8ec
Dustin Brett <dustinbrett@gmail.com>
2023-07-14 12:36:23 -0700

    Tweak test

--- e2e/components/system/Desktop/index.spec.ts
+++ e2e/components/system/Desktop/index.spec.ts
@@ -5,7 +5,7 @@ const APOD_URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
 
 const RIGHT_CLICK = { button: "right" } as Parameters<Locator["click"]>[0];
 
-const CANVAS_SELECTOR = "main>canvas";
+const BACKGROUND_CANVAS_SELECTOR = "main>canvas";
 const FIRST_FILE_ENTRY_SELECTOR = "main>ol>li:first-child";
 const CONTEXT_MENU_SELECTOR = "#__next>nav";
 
@@ -14,7 +14,7 @@ const SCREEN_CAPTURE_NOT_SUPPORTED_BROWSERS = new Set(["webkit"]);
 test("has background", async ({ page }) => {
   await page.goto("/");
 
-  await expect(page.locator(CANVAS_SELECTOR)).toHaveCount(1);
+  await expect(page.locator(BACKGROUND_CANVAS_SELECTOR)).toHaveCount(1);
 });
 
 test("has file entry", async ({ page }) => {
@@ -28,21 +28,16 @@ test("has context menu", async ({ browserName, page }) => {
 
   await page.locator("main").click(RIGHT_CLICK);
 
-  const menu = page.locator(CONTEXT_MENU_SELECTOR);
-
-  await expect(menu).toHaveCount(1);
-
-  const menuItems = menu.locator("ol>li");
+  const menuItems = page.locator(CONTEXT_MENU_SELECTOR).locator("ol>li");
 
   await expect(menuItems.getByText("Background")).toHaveCount(1);
   await expect(menuItems.getByText("View page source")).toHaveCount(1);
   await expect(menuItems.getByText("Inspect")).toHaveCount(1);
 
-  const captureScreen = menuItems.getByText("Capture screen");
-
-  await expect(captureScreen).toHaveCount(
+  await expect(menuItems.getByText("Capture screen")).toHaveCount(
     SCREEN_CAPTURE_NOT_SUPPORTED_BROWSERS.has(browserName) ? 0 : 1
   );
+
   await expect(menuItems.getByText("Properties")).toHaveCount(0);
 });
 
@@ -52,10 +47,11 @@ test("can change background", async ({ page }) => {
   const responsePromise = page.waitForResponse(APOD_URL);
 
   await page.locator("main").click(RIGHT_CLICK);
+
   await page.getByText("Background").click();
   await page.getByText("APOD").click();
 
-  await expect(page.locator(CANVAS_SELECTOR)).toHaveCount(0);
+  await expect(page.locator(BACKGROUND_CANVAS_SELECTOR)).toHaveCount(0);
 
-  expect((await responsePromise).ok()).toBe(true);
+  expect((await responsePromise).ok()).toBeTruthy();
 });
