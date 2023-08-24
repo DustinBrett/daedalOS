eb2acaf04d0437d73d7a7133eb1811c93fd1e20e
Dustin Brett <dustinbrett@gmail.com>
2023-08-23 21:27:37 -0700

    Rename constant

--- e2e/components/system/Taskbar.spec.ts
+++ e2e/components/system/Taskbar.spec.ts
@@ -38,7 +38,7 @@ import {
   windowIsTransparent,
 } from "e2e/functions";
 
-const CONTEXT_MENU_TEST = "has context menu";
+const HAS_CONTEXT_MENU = "has context menu";
 
 test.beforeEach(disableWallpaper);
 
@@ -57,7 +57,7 @@ test.describe("elements", () => {
       await sheepIsVisible({ page });
     });
 
-    test(CONTEXT_MENU_TEST, async ({ page }) => {
+    test(HAS_CONTEXT_MENU, async ({ page }) => {
       await clickStartButton({ page }, true);
       await contextMenuIsVisible({ page });
       await contextMenuHasCount(START_BUTTON_MENU_ITEMS.length, { page });
@@ -90,7 +90,7 @@ test.describe("elements", () => {
       await calendarIsVisible({ page });
     });
 
-    test(CONTEXT_MENU_TEST, async ({ page }) => {
+    test(HAS_CONTEXT_MENU, async ({ page }) => {
       await clickClock({ page }, 1, true);
       await contextMenuIsVisible({ page });
       await contextMenuHasCount(CLOCK_MENU_ITEMS.length, { page });
@@ -120,7 +120,7 @@ test.describe("entries", () => {
     test("has tooltip", async ({ page }) =>
       taskbarEntryHasTooltip(TEST_APP_TITLE, TEST_APP_TITLE, { page }));
 
-    test.describe(CONTEXT_MENU_TEST, () => {
+    test.describe(HAS_CONTEXT_MENU, () => {
       test.beforeEach(async ({ page }) => {
         await clickTaskbarEntry(TEST_APP_TITLE, { page }, true);
         await contextMenuIsVisible({ page });
@@ -171,7 +171,7 @@ test.describe("entries", () => {
     // TEST: has peek
   });
 
-  test.describe(CONTEXT_MENU_TEST, () => {
+  test.describe(HAS_CONTEXT_MENU, () => {
     test.beforeEach(async ({ page }) => {
       await clickTaskbar({ page }, true);
       await contextMenuIsVisible({ page });
