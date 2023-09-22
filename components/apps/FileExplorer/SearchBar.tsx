6c5b54c8c253612bb3ba7a576649c08b28a1505f
Dustin Brett <dustinbrett@gmail.com>
2023-09-21 23:10:54 -0700

    More dynamic search

--- components/apps/FileExplorer/SearchBar.tsx
+++ components/apps/FileExplorer/SearchBar.tsx
@@ -1,17 +1,28 @@
-import { basename } from "path";
+import { basename, join } from "path";
 import { memo, useEffect, useRef, useState } from "react";
 import { Search } from "components/apps/FileExplorer/NavigationIcons";
 import StyledSearch from "components/apps/FileExplorer/StyledSearch";
 import { TEXT_EDITORS } from "components/system/Files/FileEntry/extensions";
-import {
-  getIconByFileExtension,
-  getProcessByFileExtension,
-} from "components/system/Files/FileEntry/functions";
+import { getInfoWithExtension } from "components/system/Files/FileEntry/functions";
 import { useMenu } from "contexts/menu";
 import type { MenuItem } from "contexts/menu/useMenuContextState";
 import { useProcesses } from "contexts/process";
-import { getExtension, preloadLibs } from "utils/functions";
+import {
+  bufferToUrl,
+  getExtension,
+  isYouTubeUrl,
+  preloadLibs,
+} from "utils/functions";
 import { SEARCH_LIBS, useSearch } from "utils/search";
+import { useFileSystem } from "contexts/fileSystem";
+import type { FSModule } from "browserfs/dist/node/core/FS";
+import type { FileInfo } from "components/system/Files/FileEntry/useFileInfo";
+import {
+  ICON_CACHE,
+  ICON_CACHE_EXTENSION,
+  SHORTCUT_EXTENSION,
+  YT_ICON_CACHE,
+} from "utils/constants";
 
 type SearchBarProps = {
   id: string;
@@ -31,39 +42,68 @@ const SearchBar: FC<SearchBarProps> = ({ id }) => {
   const searchBarRef = useRef<HTMLInputElement | null>(null);
   const results = useSearch(searchTerm);
   const { contextMenu } = useMenu();
+  const { exists, fs, readFile } = useFileSystem();
 
   useEffect(() => {
     if (searchBarRef.current && hasUsedSearch.current) {
-      const getItems = (): MenuItem[] =>
-        [
-          ...results.filter(({ ref: path }) => path.startsWith(url)),
-          ...results.filter(({ ref: path }) => !path.startsWith(url)),
-        ]
-          .slice(0, MAX_ENTRIES - 1)
-          .map(({ ref: path }) => ({
-            action: () => {
-              open(
-                getProcessByFileExtension(getExtension(path)) ||
-                  TEXT_EDITORS[0],
-                { url: path }
+      const getItems = (): Promise<MenuItem[]> =>
+        Promise.all(
+          [
+            ...results.filter(({ ref: path }) => path.startsWith(url)),
+            ...results.filter(({ ref: path }) => !path.startsWith(url)),
+          ]
+            .slice(0, MAX_ENTRIES - 1)
+            .map(async ({ ref: path }) => {
+              const {
+                icon,
+                pid = TEXT_EDITORS[0],
+                url: infoUrl,
+              } = await new Promise<FileInfo>((resolve) => {
+                getInfoWithExtension(
+                  fs as FSModule,
+                  path,
+                  getExtension(path),
+                  (fileInfo) => resolve(fileInfo)
+                );
+              });
+              const isYT = isYouTubeUrl(infoUrl);
+              const cachedIconPath = join(
+                isYT ? YT_ICON_CACHE : ICON_CACHE,
+                `${
+                  isYT ? new URL(infoUrl).pathname.replace("/", "") : infoUrl
+                }${ICON_CACHE_EXTENSION}`
               );
-              setSearchTerm("");
+              let cachedIcon = "";
 
-              if (searchBarRef.current) {
-                searchBarRef.current.value = "";
-                searchBarRef.current.blur();
+              if (await exists(cachedIconPath)) {
+                cachedIcon = bufferToUrl(await readFile(cachedIconPath));
               }
-            },
-            icon: getIconByFileExtension(getExtension(path)),
-            label: basename(path),
-          }));
 
-      contextMenu?.(getItems).onContextMenuCapture(
-        undefined,
-        searchBarRef.current.getBoundingClientRect()
+              return {
+                action: () => {
+                  open(pid, { url: infoUrl || path });
+                  setSearchTerm("");
+
+                  if (searchBarRef.current) {
+                    searchBarRef.current.value = "";
+                    searchBarRef.current.blur();
+                  }
+                },
+                icon: cachedIcon || icon,
+                label: basename(path, SHORTCUT_EXTENSION),
+              };
+            })
+        );
+
+      getItems().then(
+        (items) =>
+          contextMenu?.(() => items).onContextMenuCapture(
+            undefined,
+            searchBarRef.current?.getBoundingClientRect()
+          )
       );
     }
-  }, [contextMenu, open, results, url]);
+  }, [contextMenu, exists, fs, open, readFile, results, url]);
 
   useEffect(() => {
     if (searchBarRef.current) {
