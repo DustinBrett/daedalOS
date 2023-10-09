0f3a701d797dcd2f0cc57352d7aa359091f845ba
Dustin Brett <dustinbrett@gmail.com>
2023-10-09 10:06:57 -0700

    tinymce dark mode improvements

--- components/apps/TinyMCE/StyledTinyMceEditor.ts
+++ components/apps/TinyMCE/StyledTinyMceEditor.ts
@@ -20,16 +20,17 @@ const StyledTinyMceEditor = styled.div`
 
         &::before {
           background-color: rgba(42, 43, 46, 90%);
-          color: #fff;
-          content: "Click to switch into design mode.";
+          color: rgb(225, 225, 225);
+          content: "Click to switch into design mode";
           cursor: pointer;
           display: flex;
+          font-size: 14px;
           font-weight: 600;
           height: calc(100% - 1px);
           place-content: center;
           place-items: center;
           position: absolute;
-          text-shadow: 0 0 10px rgba(0, 0, 0, 100%);
+          text-shadow: 0 0 10px rgba(255, 255, 255, 50%);
           width: 100%;
           z-index: 1;
         }
@@ -38,6 +39,10 @@ const StyledTinyMceEditor = styled.div`
           display: none;
         }
       }
+    }
+
+    iframe {
+      background-color: #202124;
     }
   }
 `;
