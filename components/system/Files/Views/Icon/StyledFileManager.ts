2053f797e5f3c806edc4de6b2e2a771221bef5db
Dustin Brett <dustinbrett@gmail.com>
2023-10-20 16:46:01 -0700

    Pkg upgrades

--- components/system/Files/Views/Icon/StyledFileManager.ts
+++ components/system/Files/Views/Icon/StyledFileManager.ts
@@ -6,9 +6,10 @@ import { TASKBAR_HEIGHT } from "utils/constants";
 const StyledFileManager = styled.ol<StyledFileManagerProps>`
   ${({ $scrollable }) => ($scrollable ? ScrollBars() : undefined)};
 
-  column-gap: ${({ theme }) => theme.sizes.fileManager.columnGap};
   contain: strict;
   display: grid;
+  gap: ${({ theme }) =>
+    `${theme.sizes.fileManager.rowGap} ${theme.sizes.fileManager.columnGap}`};
   grid-auto-flow: row;
   grid-template-columns: ${({ theme }) =>
     `repeat(auto-fill, ${theme.sizes.fileManager.gridEntryWidth})`};
@@ -19,7 +20,6 @@ const StyledFileManager = styled.ol<StyledFileManagerProps>`
   padding: ${({ theme }) => theme.sizes.fileManager.padding};
   place-content: flex-start;
   pointer-events: ${({ $selecting }) => ($selecting ? "auto" : undefined)};
-  row-gap: ${({ theme }) => theme.sizes.fileManager.rowGap};
 
   main > & {
     grid-auto-flow: column;
