23f99ca0f221a8ac1cb268b8207309c0320a1e81
Thomas Stephens <tmsteph1290@gmail.com>
2023-11-22 22:16:44 -0800

    Update index.tsx

--- components/system/Desktop/index.tsx
+++ components/system/Desktop/index.tsx
@@ -20,7 +20,7 @@ const Desktop: FC = ({ children }) => {
     right: "10px",
     textShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
   };
-  
+
   return (
     <StyledDesktop ref={desktopRef} $height={heightOverride}>
       <div style={desktopNameStyle}>tommyos</div>
