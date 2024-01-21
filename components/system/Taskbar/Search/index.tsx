e8184e4e0d6733e75203b4eac2bcfc8e90a93968
Dustin Brett <dustinbrett@gmail.com>
2024-01-20 17:47:51 -0800

    More unique key in recent history

--- components/system/Taskbar/Search/index.tsx
+++ components/system/Taskbar/Search/index.tsx
@@ -335,7 +335,7 @@ const Search: FC<SearchProps> = ({ toggleSearch }) => {
                       {recentFiles.map(([file, pid, title], index) => (
                         // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
                         <li
-                          key={file}
+                          key={`${file}${pid}`}
                           onClick={() => {
                             openApp(pid, { url: file });
                             if (index !== 0) {
