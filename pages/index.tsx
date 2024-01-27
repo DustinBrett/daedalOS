b5daee6cb47d802f0093a61995ee8a4620b1db0b
Dustin Brett <dustinbrett@gmail.com>
2024-01-27 09:34:49 -0800

    Add temp darkreader exception

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useEffect } from "react";
 import AppsLoader from "components/system/Apps/AppsLoader";
 import Desktop from "components/system/Desktop";
 import Taskbar from "components/system/Taskbar";
@@ -7,11 +7,26 @@ import useGlobalKeyboardShortcuts from "hooks/useGlobalKeyboardShortcuts";
 import useIFrameFocuser from "hooks/useIFrameFocuser";
 import useUrlLoader from "hooks/useUrlLoader";
 
+const useExtensionDisabler = (): void => {
+  useEffect(() => {
+    // https://github.com/darkreader/darkreader/blob/main/CONTRIBUTING.md#disabling-dark-reader-on-your-site
+    // TODO: Remove once in https://github.com/darkreader/darkreader/blob/main/src/config/dark-sites.config
+    if ("darkreaderMode" in document.documentElement.dataset) {
+      const metaDarkReaderOff = document.createElement("meta");
+
+      metaDarkReaderOff.name = "darkreader-lock";
+
+      document.head.append(metaDarkReaderOff);
+    }
+  }, []);
+};
+
 const Index = (): React.ReactElement => {
   useIFrameFocuser();
   useUrlLoader();
   useGlobalKeyboardShortcuts();
   useGlobalErrorHandler();
+  useExtensionDisabler();
 
   return (
     <Desktop>
