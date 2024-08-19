78589580da36d071489d3c37c1f45db298a8afb2
Dustin Brett <dustinbrett@gmail.com>
2024-08-18 19:21:39 -0700

    Less blurry for some windows after rnd

--- components/system/Taskbar/TaskbarEntry/Peek/StyledPeekViewport.ts
+++ components/system/Taskbar/TaskbarEntry/Peek/StyledPeekViewport.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const StyledPeekViewport = styled.div`
   background-color: inherit;
   height: inherit;
+  transform: translate3d(0, 0, 0);
   width: inherit;
 `;
 
