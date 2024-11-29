3cfa754562af096788b4af3e54825c988a5fca6e
Henry <henry.arcangello@proton.me>
2024-11-29 18:28:46 +0000

    asasd

--- components/system/StartMenu/Sidebar/StyledSidebar.ts
+++ components/system/StartMenu/Sidebar/StyledSidebar.ts
@@ -8,7 +8,7 @@ const StyledSidebar = styled.nav`
   overflow: hidden;
   padding-top: 4px;
   position: absolute;
-  top: 0;
+  top: 20px;
   transition-duration: 150ms;
   width: ${({ theme }) => theme.sizes.startMenu.sideBar.width}px;
   z-index: 1;
