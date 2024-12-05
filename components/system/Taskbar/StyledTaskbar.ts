6aa84067747f36641d90e085e560a0b7551e4b07
Henry <henry.arcangello@proton.me>
2024-12-05 10:04:34 +0000

    update components/system/Taskbar/StyledTaskbar.ts

--- components/system/Taskbar/StyledTaskbar.ts
+++ components/system/Taskbar/StyledTaskbar.ts
@@ -2,13 +2,13 @@ import styled from "styled-components";
 import { TASKBAR_HEIGHT } from "utils/constants";
 
 const StyledTaskbar = styled.nav`
-  backdrop-filter: ${({ theme }) => `blur(${theme.sizes.taskbar.blur})`};
-  background-color: ${({ theme }) => theme.colors.taskbar.background};
+
+  background-color: rgba(0,0,0,0.8);
   bottom: 0;
   contain: size layout;
   height: ${TASKBAR_HEIGHT}px;
   left: 0;
-  position: absolute;
+  position: fixed;
   right: 0;
   top: 0vh;
   width: 100vw;
