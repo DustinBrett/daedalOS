8198e0a61a0d43dbe8f581dd2e4cf7d711b2165a
Dustin Brett <dustinbrett@gmail.com>
2025-01-20 19:57:36 -0800

    Error handler out of scope

--- hooks/useGlobalErrorHandler.ts
+++ hooks/useGlobalErrorHandler.ts
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
 import { haltEvent } from "utils/functions";
 
+const errorHandler = (event: Event & { error: Error }): void => {
+  if (event.error?.stack?.includes("eval at <anonymous>")) {
+    haltEvent(event);
+  }
+};
+
 const useGlobalErrorHandler = (): void => {
   useEffect(() => {
-    const errorHandler = (event: Event & { error: Error }): void => {
-      if (event.error?.stack?.includes("eval at <anonymous>")) {
-        haltEvent(event);
-      }
-    };
-
     window.addEventListener("error", errorHandler);
 
     return () => window.removeEventListener("error", errorHandler);
